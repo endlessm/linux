539c894f5510f17bb9b43a47714b165a280923a9
Scott James Remnant <scott@canonical.com>
2009-03-16 15:03:07 +0000

    UBUNTU: SAUCE: (no-up) nbd: Change default partitions per device to 15
    This was previously changed by using an "options" line in a modprobe.d
    file, however that practice is now deprecated.  This is because module
    names, option names, their values and even their current defaults can
    all change inside the kernel and module-init-tools has never been kept
    in sync.
    
    In addition, changing the kernel means that the option change will apply
    if the module is built in by users or the OEM team.
    
    Bug: #342563
    
    Signed-off-by: Scott James Remnant <scott@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/block/nbd.c
+++ drivers/block/nbd.c
@@ -128,7 +128,7 @@ static struct dentry *nbd_dbg_dir;
 #define NBD_MAGIC 0x68797548
 
 static unsigned int nbds_max = 16;
-static int max_part;
+static int max_part = 15;
 static struct workqueue_struct *recv_workqueue;
 static int part_shift;
 
