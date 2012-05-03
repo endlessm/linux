8dae516f713a7deb3bb58778ab8a07fe321d4d42
Andy Whitcroft <apw@canonical.com>
2012-05-03 16:01:39 +0100

    UBUNTU: SAUCE: (no-up) kconfig: in debug mode some 0 length message prints occur
    Has no real kernel impact, so continue to carry.
    
    When we enable the zconfdump() debugging we see assertion failures
    attempting to print the config.  Convert this into a noop.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- scripts/kconfig/lkc.h
+++ scripts/kconfig/lkc.h
@@ -98,7 +98,9 @@ struct conf_printer {
 /* confdata.c and expr.c */
 static inline void xfwrite(const void *str, size_t len, size_t count, FILE *out)
 {
-	assert(len != 0);
+	//assert(len != 0);
+	if (len == 0)
+		return;
 
 	if (fwrite(str, len, count, out) != count)
 		fprintf(stderr, "Error in writing or end of file.\n");
