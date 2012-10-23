cd0ccf9d2c45f898df225086b0fa7bc933294350
Dave Howells <dhowells@redhat.com>
2012-10-23 09:30:54 -0400

    UBUNTU: SAUCE: UEFI: Add EFI signature data types
    Add the data types that are used for containing hashes, keys and certificates
    for cryptographic verification.
    
    Bugzilla: N/A
    Upstream-status: Fedora mustard for now
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- include/linux/efi.h
+++ include/linux/efi.h
@@ -600,6 +600,12 @@ void efi_native_runtime_setup(void);
 #define LINUX_EFI_ARM_SCREEN_INFO_TABLE_GUID	EFI_GUID(0xe03fc20a, 0x85dc, 0x406e,  0xb9, 0x0e, 0x4a, 0xb5, 0x02, 0x37, 0x1d, 0x95)
 #define LINUX_EFI_LOADER_ENTRY_GUID		EFI_GUID(0x4a67b082, 0x0a4c, 0x41cf,  0xb6, 0xc7, 0x44, 0x0b, 0x29, 0xbb, 0x8c, 0x4f)
 
+#define EFI_CERT_SHA256_GUID \
+    EFI_GUID(  0xc1c41626, 0x504c, 0x4092, 0xac, 0xa9, 0x41, 0xf9, 0x36, 0x93, 0x43, 0x28 )
+
+#define EFI_CERT_X509_GUID \
+    EFI_GUID(  0xa5c059a1, 0x94e4, 0x4aa7, 0x87, 0xb5, 0xab, 0x15, 0x5c, 0x2b, 0xf0, 0x72 )
+
 typedef struct {
 	efi_guid_t guid;
 	u64 table;
@@ -849,6 +855,20 @@ typedef struct {
 	u32 reserved;
 	efi_memory_desc_t entry[0];
 } efi_memory_attributes_table_t;
+
+typedef struct  {
+	efi_guid_t signature_owner;
+	u8 signature_data[];
+} efi_signature_data_t;
+
+typedef struct {
+	efi_guid_t signature_type;
+	u32 signature_list_size;
+	u32 signature_header_size;
+	u32 signature_size;
+	u8 signature_header[];
+	/* efi_signature_data_t signatures[][] */
+} efi_signature_list_t;
 
 /*
  * All runtime access to EFI goes through this structure:
