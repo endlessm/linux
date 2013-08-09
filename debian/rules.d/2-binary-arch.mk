1dfce2b0fdfc6197a1fc31e5de9ea359fa208558
Kamal Mostafa <kamal@canonical.com>
2013-08-09 16:03:46 -0700

    UBUNTU: [debian] tools: ship 'cpupower' in linux-tools
    BugLink: http://bugs.launchpad.net/bugs/1158668
    
    New Build-depends: libpci-dev
    
    Adds to binary package "linux-tools-$(abi_version)"
      /usr/bin/cpupower_$(abi_version)
      /usr/lib/libcpupower.so.$(abi_version)
    
    Adds to binary package "linux-tools-common"
      /usr/bin/cpupower
      /usr/share/man/man1/cpupower-set.1.gz
      /usr/share/man/man1/cpupower-frequency-set.1.gz
      /usr/share/man/man1/cpupower-frequency-info.1.gz
      /usr/share/man/man1/cpupower-monitor.1.gz
      /usr/share/man/man1/cpupower-info.1.gz
      /usr/share/man/man1/cpupower-idle-info.1.gz
      /usr/share/man/man1/cpupower.1.gz
    
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -458,6 +458,14 @@ endif
 $(stampdir)/stamp-build-perarch: $(stampdir)/stamp-prepare-perarch
 	@echo Debug: $@
 ifeq ($(do_tools),true)
+
+	# Allow for multiple installed versions of cpupower and libcpupower.so:
+	# Override LIB_MIN in order to to generate a versioned .so named
+	# libcpupower.so.$(abi_release) and link cpupower with that.
+	make -C $(builddirpa)/tools/power/cpupower \
+		CROSS_COMPILE=$(CROSS_COMPILE) \
+		LIB_MIN=$(abi_release) CPUFREQ_BENCH=false
+
 ifeq ($(do_tools_perf),true)
 	cd $(builddirpa)/tools/perf && \
 		make prefix=/usr HAVE_CPLUS_DEMANGLE=1 CROSS_COMPILE=$(CROSS_COMPILE) NO_LIBPYTHON=1 NO_LIBPERL=1 PYTHON=python2.7
@@ -478,6 +486,15 @@ install-perarch: $(stampdir)/stamp-build-perarch
 	# Add the tools.
 ifeq ($(do_tools),true)
 	install -d $(toolspkgdir)/usr/bin
+	install -d $(toolspkgdir)/usr/lib
+
+	install -m755 $(builddirpa)/tools/power/cpupower/cpupower \
+		$(toolspkgdir)/usr/bin/cpupower_$(abi_release)
+	# Install only the full versioned libcpupower.so.$(abi_release), not
+	# the usual symlinks to it.
+	install -m644 $(builddirpa)/tools/power/cpupower/libcpupower.so.$(abi_release) \
+		$(toolspkgdir)/usr/lib/
+
 ifeq ($(do_tools_perf),true)
 	install -m755 $(builddirpa)/tools/perf/perf \
 		$(toolspkgdir)/usr/bin/perf_$(abi_release)
