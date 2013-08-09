562d2183df71c176ad1d7fa7caa214e53cb46565
Matthew Garrett <matthew.garrett@nebula.com>
2013-08-09 17:58:15 -0400

    UBUNTU: SAUCE: UEFI: Add secure_modules() call
    Provide a single call to allow kernel code to determine whether the system
    has been configured to either disable module loading entirely or to load
    only modules signed with a trusted key.
    
    Bugzilla: N/A
    Upstream-status: Fedora mustard.  Replaced by securelevels, but that was nak'd
    
    Signed-off-by: Matthew Garrett <matthew.garrett@nebula.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- kernel/module.c
+++ kernel/module.c
@@ -4299,3 +4299,13 @@ void module_layout(struct module *mod,
 }
 EXPORT_SYMBOL(module_layout);
 #endif
+
+bool secure_modules(void)
+{
+#ifdef CONFIG_MODULE_SIG
+	return (sig_enforce || modules_disabled);
+#else
+	return modules_disabled;
+#endif
+}
+EXPORT_SYMBOL(secure_modules);
