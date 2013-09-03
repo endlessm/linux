8a315fa270ceb50b07813a9970fe83facbd6553b
Matthew Garrett <mjg59@srcf.ucam.org>
2013-09-03 11:23:29 -0400

    uswsusp: Disable when module signing is enforced
    uswsusp allows a user process to dump and then restore kernel state, which
    makes it possible to modify the running kernel. Disable this if module
    signing is enforced.
    
    Signed-off-by: Matthew Garrett <mjg59@srcf.ucam.org>
    
    Based on a patch by Matthew which used securelevel instead.
    
    Signed-off-by: João Paulo Rechi Vita <jprvita@endlessm.com>

--- kernel/power/user.c
+++ kernel/power/user.c
@@ -24,6 +24,7 @@
 #include <linux/console.h>
 #include <linux/cpu.h>
 #include <linux/freezer.h>
+#include <linux/module.h>
 
 #include <linux/uaccess.h>
 
@@ -50,6 +51,9 @@ static int snapshot_open(struct inode *inode, struct file *filp)
 	int error, nr_calls = 0;
 
 	if (!hibernation_available())
+		return -EPERM;
+
+	if (secure_modules())
 		return -EPERM;
 
 	lock_system_sleep();
