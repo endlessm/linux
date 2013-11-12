a0e85210ddc15e3aca978da5d64a907985f1b5f7
Andy Whitcroft <apw@canonical.com>
2013-11-12 13:58:07 +0000

    UBUNTU: [Config] ppc64el -- create linux-libc-dev
    Generate the linux-libc-dev package for ppc64el for cross-compilation
    support.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian.master/rules.d/ppc64el.mk
+++ debian.master/rules.d/ppc64el.mk
@@ -1,7 +1,7 @@
 human_arch	= PowerPC 64el
 build_arch	= powerpc
 header_arch	= $(build_arch)
-defconfig	= ppc64_defconfig
+defconfig	= pseries_le_defconfig
 flavours	=
 build_image	= vmlinux
 kernel_file	= $(build_image)
