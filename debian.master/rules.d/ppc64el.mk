da005ca6bfceabd6359199aac99231552b119ce3
Andy Whitcroft <apw@canonical.com>
2013-12-02 17:52:41 +0000

    UBUNTU: [Config] ppc64el -- initial defconfig based -generic flavour
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian.master/rules.d/ppc64el.mk
+++ debian.master/rules.d/ppc64el.mk
@@ -2,12 +2,12 @@ human_arch	= PowerPC 64el
 build_arch	= powerpc
 header_arch	= $(build_arch)
 defconfig	= pseries_le_defconfig
-flavours	=
-build_image	= vmlinux
-kernel_file	= $(build_image)
-install_file	= $(build_image)
+flavours	= generic
+build_image	= vmlinux.strip
+kernel_file     = arch/powerpc/boot/vmlinux.strip
+install_file	= vmlinux
 no_dumpfile	= true
 loader		= yaboot
 
 do_tools	= false
-do_flavour_image_package = false
+#do_flavour_image_package = false
