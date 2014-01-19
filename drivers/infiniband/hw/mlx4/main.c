b8a22a871cf25fe94e909c7b9da5e1878b16ac37
Roland Dreier <roland@purestorage.com>
2014-01-19 15:18:49 -0800

    IB/mlx4: Use IS_ENABLED(CONFIG_IPV6)
    ...instead of testing defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
    
    Signed-off-by: Roland Dreier <roland@purestorage.com>
    (cherry picked from commit 27cdef637c25705b433d5c4deeef4cf8dcb75d6a)
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/infiniband/hw/mlx4/main.c
+++ drivers/infiniband/hw/mlx4/main.c
@@ -1569,7 +1569,7 @@ static int mlx4_ib_inet_event(struct notifier_block *this, unsigned long event,
 	return NOTIFY_DONE;
 }
 
-#if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
+#if IS_ENABLED(CONFIG_IPV6)
 static int mlx4_ib_inet6_event(struct notifier_block *this, unsigned long event,
 				void *ptr)
 {
@@ -1589,7 +1589,7 @@ static void mlx4_ib_get_dev_addr(struct net_device *dev,
 				 struct mlx4_ib_dev *ibdev, u8 port)
 {
 	struct in_device *in_dev;
-#if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
+#if IS_ENABLED(CONFIG_IPV6)
 	struct inet6_dev *in6_dev;
 	union ib_gid  *pgid;
 	struct inet6_ifaddr *ifp;
@@ -1612,7 +1612,7 @@ static void mlx4_ib_get_dev_addr(struct net_device *dev,
 		endfor_ifa(in_dev);
 		in_dev_put(in_dev);
 	}
-#if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
+#if IS_ENABLED(CONFIG_IPV6)
 	/* IPv6 gids */
 	in6_dev = in6_dev_get(dev);
 	if (in6_dev) {
@@ -2041,7 +2041,7 @@ static void *mlx4_ib_add(struct mlx4_dev *dev)
 				goto err_notif;
 			}
 		}
-#if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
+#if IS_ENABLED(CONFIG_IPV6)
 		if (!iboe->nb_inet6.notifier_call) {
 			iboe->nb_inet6.notifier_call = mlx4_ib_inet6_event;
 			err = register_inet6addr_notifier(&iboe->nb_inet6);
@@ -2088,7 +2088,7 @@ err_notif:
 			pr_warn("failure unregistering notifier\n");
 		ibdev->iboe.nb_inet.notifier_call = NULL;
 	}
-#if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
+#if IS_ENABLED(CONFIG_IPV6)
 	if (ibdev->iboe.nb_inet6.notifier_call) {
 		if (unregister_inet6addr_notifier(&ibdev->iboe.nb_inet6))
 			pr_warn("failure unregistering notifier\n");
@@ -2220,7 +2220,7 @@ static void mlx4_ib_remove(struct mlx4_dev *dev, void *ibdev_ptr)
 			pr_warn("failure unregistering notifier\n");
 		ibdev->iboe.nb_inet.notifier_call = NULL;
 	}
-#if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
+#if IS_ENABLED(CONFIG_IPV6)
 	if (ibdev->iboe.nb_inet6.notifier_call) {
 		if (unregister_inet6addr_notifier(&ibdev->iboe.nb_inet6))
 			pr_warn("failure unregistering notifier\n");
