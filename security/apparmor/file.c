611fd3da044abb19f436cf054496f09bc4c24dca
John Johansen <john.johansen@canonical.com>
2014-03-02 01:52:54 -0800

    Revert "UBUNTU: SAUCE: apparmor: fix uninitialized lsm_audit membe"
    This reverts commit 406460e3f35e1d02f3c531e0fe41cc978184192d.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- security/apparmor/file.c
+++ security/apparmor/file.c
@@ -110,8 +110,7 @@ int aa_audit_file(struct aa_profile *profile, struct file_perms *perms,
 	int type = AUDIT_APPARMOR_AUTO;
 	struct common_audit_data sa;
 	struct apparmor_audit_data aad = {0,};
-	sa.type = LSM_AUDIT_DATA_TASK;
-	sa.u.tsk = NULL;
+	sa.type = LSM_AUDIT_DATA_NONE;
 	aad_set(&sa, &aad);
 	aad.op = op,
 	aad.fs.request = request;
