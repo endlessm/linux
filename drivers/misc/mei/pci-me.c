90bed38491231f9371939fcc012183bf46207df2
Tim Gardner <tim.gardner@canonical.com>
2014-03-06 10:20:15 -0700

    UBUNTU: SAUCE: (no-up) mei_me: Add module parameter to disable MSI
    mei_me.disable_msi = 1
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/misc/mei/pci-me.c
+++ drivers/misc/mei/pci-me.c
@@ -23,6 +23,9 @@
 #include "hw-me-regs.h"
 #include "hw-me.h"
 
+static bool disable_msi;
+module_param(disable_msi, bool, 0);
+
 /* mei_pci_tbl - PCI Device ID Table */
 static const struct pci_device_id mei_me_pci_tbl[] = {
 	{MEI_PCI_DEVICE(MEI_DEV_ID_82946GZ, MEI_ME_ICH_CFG)},
@@ -214,7 +217,8 @@ static int mei_me_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	hw->mem_addr = pcim_iomap_table(pdev)[0];
 	hw->read_fws = mei_me_read_fws;
 
-	pci_enable_msi(pdev);
+	if (!disable_msi)
+		pci_enable_msi(pdev);
 
 	hw->irq = pdev->irq;
 
