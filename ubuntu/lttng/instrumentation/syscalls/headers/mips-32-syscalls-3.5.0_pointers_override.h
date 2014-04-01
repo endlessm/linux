83b774c66bc9f7bb000b0b7c74091aa9b4a2829f
Tim Gardner <tim.gardner@canonical.com>
2014-04-01 08:35:20 -0600

    UBUNTU: SAUCE: Drop lttng in favor of lttng-modules
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- ubuntu/lttng/instrumentation/syscalls/headers/mips-32-syscalls-3.5.0_pointers_override.h
+++
@@ -1,8 +0,0 @@
-#ifndef CREATE_SYSCALL_TABLE
-
-#else	/* CREATE_SYSCALL_TABLE */
-
-#define OVERRIDE_TABLE_32_sys_clone
-TRACE_SYSCALL_TABLE(sys_clone, sys_clone, 4120, 0)
-
-#endif /* CREATE_SYSCALL_TABLE */
--- ubuntu/lttng/instrumentation/syscalls/headers/mips-64-syscalls-3.5.0_integers_override.h
+++
@@ -1,3 +0,0 @@
-/*
- * this is a place-holder for MIPS integer syscall definition override.
- */
