e87828ddb417592f2261c82af4600e32e8aa5eac
dann frazier <dann.frazier@canonical.com>
2014-04-01 18:43:09 -0600

    UBUNTU: [Config] enable linux-tools on arm64
    perf smoke tested and proven functional. cpupower builds but isn't terribly
    useful w/o CPUFREQ it appears. I'm assuming it is sane to pre-enable it so
    that this support is available should kernel SRUs enable the underlying
    kernel support.
    
    Signed-off-by: dann frazier <dann.frazier@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian.master/rules.d/arm64.mk
+++ debian.master/rules.d/arm64.mk
@@ -10,6 +10,9 @@ no_dumpfile = true
 
 loader		= grub
 
+do_tools_cpupower = true
+do_tools_perf	= true
+
 dtb_files_generic += apm-mustang.dtb
 dtb_files_generic += foundation-v8.dtb
 dtb_files_generic += rtsm_ve-aemv8a.dtb
