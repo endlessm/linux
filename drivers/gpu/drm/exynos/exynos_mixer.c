efe6e4d95c79f6683160e45691eed37841c36753
Derek Foreman <derek.foreman@collabora.co.uk>
2014-04-03 14:10:30 -0500

    exynos-drm: mixer: Obey buffer pitch
    The exynos mixer's "span" should really use pitch - not width.
    Needed for overscan compensation to work right.

--- drivers/gpu/drm/exynos/exynos_mixer.c
+++ drivers/gpu/drm/exynos/exynos_mixer.c
@@ -58,6 +58,7 @@ struct hdmi_win_data {
 	unsigned int		fb_y;
 	unsigned int		fb_width;
 	unsigned int		fb_height;
+	unsigned int		fb_pitch;
 	unsigned int		src_width;
 	unsigned int		src_height;
 	unsigned int		mode_width;
@@ -573,7 +574,7 @@ static void mixer_graph_buffer(struct mixer_context *ctx, int win)
 		MXR_GRP_CFG_FORMAT_VAL(fmt), MXR_GRP_CFG_FORMAT_MASK);
 
 	/* setup geometry */
-	mixer_reg_write(res, MXR_GRAPHIC_SPAN(win), win_data->fb_width);
+	mixer_reg_write(res, MXR_GRAPHIC_SPAN(win), win_data->fb_pitch / (win_data->bpp / 8));
 
 	/* setup display size */
 	if (ctx->mxr_ver == MXR_VER_128_0_0_184 &&
@@ -955,6 +956,7 @@ static void mixer_win_mode_set(struct exynos_drm_manager *mgr,
 	win_data->fb_y = overlay->fb_y;
 	win_data->fb_width = overlay->fb_width;
 	win_data->fb_height = overlay->fb_height;
+	win_data->fb_pitch = overlay->pitch;
 	win_data->src_width = overlay->src_width;
 	win_data->src_height = overlay->src_height;
 
