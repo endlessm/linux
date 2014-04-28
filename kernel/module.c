39873b290e01ba34bb645b76239c828ee840abfc
Rusty Russell <rusty@rustcorp.com.au>
2014-04-28 11:05:43 +0930

    module: remove warning about waiting module removal.
    BugLink: http://bugs.launchpad.net/bugs/1333837
    
    commit 79465d2fd48e68940c2bdecddbdecd45bbba06fe upstream.
    
    We remove the waiting module removal in commit 3f2b9c9cdf38 (September
    2013), but it turns out that modprobe in kmod (< version 16) was
    asking for waiting module removal.  No one noticed since modprobe would
    check for 0 usage immediately before trying to remove the module, and
    the race is unlikely.
    
    However, it means that anyone running old (but not ancient) kmod
    versions is hitting the printk designed to see if anyone was running
    "rmmod -w".  All reports so far have been false positives, so remove
    the warning.
    
    Fixes: 3f2b9c9cdf389e303b2273679af08aab5f153517
    Reported-by: Valerio Vanni <valerio.vanni@inwind.it>
    Cc: Elliott, Robert (Server Storage) <Elliott@hp.com>
    Acked-by: Lucas De Marchi <lucas.de.marchi@gmail.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    [ kamal: backport to 3.13: context ]
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- kernel/module.c
+++ kernel/module.c
@@ -815,11 +815,6 @@ SYSCALL_DEFINE2(delete_module, const char __user *, name_user,
 		return -EFAULT;
 	name[MODULE_NAME_LEN-1] = '\0';
 
-	if (!(flags & O_NONBLOCK)) {
-		printk(KERN_WARNING
-		       "waiting module removal not supported: please upgrade");
-	}
-
 	if (mutex_lock_interruptible(&module_mutex) != 0)
 		return -EINTR;
 
