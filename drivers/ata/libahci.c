53b4a77cc410b45f5c83fbea85c9c7a8ee31d310
Suman Tripathi <stripathi@apm.com>
2014-06-23 16:11:25 -0600

    UBUNTU: SAUCE: (no-up) ata: Fix the dma state machine lockup for the IDENTIFY DEVICE PIO mode command.
    BugLink: http://bugs.launchpad.net/bugs/1335645
    
    This patch fixes the dma state machine lockup due to the processing
    of IDENTIFY DEVICE PIO mode command. The X-Gene AHCI controller
    has an errata in which it cannot clear the BSY bit after
    receiving the PIO setup FIS and results the dma state machine to go
    into the CMFatalErrorUpdate state resulting in the dma state
    machine lockup. This patch also removes the dma restart workaround
    from the read_id function as the read_id function is only called by
    libata layer for ATA_INTERNAL commands. But for somecases eg:
    PORT MULTIPLIER and udev, the framework will enumerate using SCSI
    commands and it will not call read_id function.
    
    Signed-off-by: Loc Ho <lho@apm.com>
    Signed-off-by: Suman Tripathi <stripathi@apm.com>
    Signed-off-by: dann frazier <dann.frazier@canonical.com>
    (v4 submittal)
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/ata/libahci.c
+++ drivers/ata/libahci.c
@@ -69,7 +69,6 @@ static ssize_t ahci_transmit_led_message(struct ata_port *ap, u32 state,
 
 static int ahci_scr_read(struct ata_link *link, unsigned int sc_reg, u32 *val);
 static int ahci_scr_write(struct ata_link *link, unsigned int sc_reg, u32 val);
-static unsigned int ahci_qc_issue(struct ata_queued_cmd *qc);
 static bool ahci_qc_fill_rtf(struct ata_queued_cmd *qc);
 static int ahci_port_start(struct ata_port *ap);
 static void ahci_port_stop(struct ata_port *ap);
@@ -1945,7 +1944,7 @@ irqreturn_t ahci_interrupt(int irq, void *dev_instance)
 }
 EXPORT_SYMBOL_GPL(ahci_interrupt);
 
-static unsigned int ahci_qc_issue(struct ata_queued_cmd *qc)
+unsigned int ahci_qc_issue(struct ata_queued_cmd *qc)
 {
 	struct ata_port *ap = qc->ap;
 	void __iomem *port_mmio = ahci_port_base(ap);
@@ -1974,6 +1973,7 @@ static unsigned int ahci_qc_issue(struct ata_queued_cmd *qc)
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(ahci_qc_issue);
 
 static bool ahci_qc_fill_rtf(struct ata_queued_cmd *qc)
 {
