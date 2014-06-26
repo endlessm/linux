d9965748ce4cb7b56f1d669b69e60ff2f5879e08
Dan Nicholson <nicholson@endlessm.com>
2014-06-26 08:56:28 -0700

    UBUNTU: Make printchanges/insertchanges work with Endless release commits
    For endless, the commit subject will be 'UBUNTU: release x.y.z' instead
    of 'UBUNTU: Ubuntu-x.y.z'. Detect that option to get the changelog
    handling to work again.
    
    [endlessm/eos-shell#2840]

--- debian/rules.d/1-maintainer.mk
+++ debian/rules.d/1-maintainer.mk
@@ -97,7 +97,7 @@ printenv:
 
 printchanges:
 	@baseCommit=$$(git log --pretty=format:'%H %s' | \
-		gawk '/UBUNTU: '".*Ubuntu-$(prev_fullver)"'$$/ { print $$1; exit }'); \
+		gawk '/UBUNTU: '".*(Ubuntu-|release )$(prev_fullver)"'$$/ { print $$1; exit }'); \
 		git log "$$baseCommit"..HEAD | \
 		perl -w -f $(DROOT)/scripts/misc/git-ubuntu-log $(ubuntu_log_opts)
 
