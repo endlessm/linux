dc513a0274fb6e811139ba7ff112f7b1257e182e
Daniel Drake <drake@endlessm.com>
2014-07-15 16:31:51 +0100

    FIMC/MFC: static device node names
    Hardcode these numbers to match the layout in the 3.8 kernel:
     s5p-fimc-md: Registered fimc.0.m2m as /dev/video10
     s5p-fimc-md: Registered fimc.0.capture as /dev/video11
     s5p-fimc-md: Registered fimc.1.m2m as /dev/video12
     s5p-fimc-md: Registered fimc.1.capture as /dev/video13
     s5p-mfc 13400000.codec: decoder registered as /dev/video8
     s5p-mfc 13400000.codec: encoder registered as /dev/video9

--- drivers/media/platform/s5p-mfc/s5p_mfc.c
+++ drivers/media/platform/s5p-mfc/s5p_mfc.c
@@ -1156,7 +1156,7 @@ static int s5p_mfc_probe(struct platform_device *pdev)
 	vfd->vfl_dir	= VFL_DIR_M2M;
 	snprintf(vfd->name, sizeof(vfd->name), "%s", S5P_MFC_DEC_NAME);
 	dev->vfd_dec	= vfd;
-	ret = video_register_device(vfd, VFL_TYPE_GRABBER, 0);
+	ret = video_register_device(vfd, VFL_TYPE_GRABBER, 8);
 	if (ret) {
 		v4l2_err(&dev->v4l2_dev, "Failed to register video device\n");
 		video_device_release(vfd);
@@ -1181,7 +1181,7 @@ static int s5p_mfc_probe(struct platform_device *pdev)
 	vfd->vfl_dir	= VFL_DIR_M2M;
 	snprintf(vfd->name, sizeof(vfd->name), "%s", S5P_MFC_ENC_NAME);
 	dev->vfd_enc	= vfd;
-	ret = video_register_device(vfd, VFL_TYPE_GRABBER, 0);
+	ret = video_register_device(vfd, VFL_TYPE_GRABBER, 9);
 	if (ret) {
 		v4l2_err(&dev->v4l2_dev, "Failed to register video device\n");
 		video_device_release(vfd);
