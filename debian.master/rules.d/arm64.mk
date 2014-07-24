d752a09e3645ca542d7536623d9aca06388aca54
Tim Gardner <tim.gardner@canonical.com>
2014-07-24 07:28:11 -0600

    UBUNTU: rebase to v4.3-rc5
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

---
+++ debian.master/rules.d/arm64.mk
@@ -0,0 +1,18 @@
+human_arch	= ARMv8
+build_arch	= arm64
+header_arch	= arm64
+defconfig	= defconfig
+flavours	= generic
+build_image	= Image
+kernel_file	= arch/$(build_arch)/boot/Image
+install_file	= vmlinuz
+no_dumpfile = true
+
+loader		= grub
+vdso		= vdso_install
+
+do_tools_usbip  = true
+do_tools_cpupower = true
+do_tools_perf   = true
+
+do_dtbs		= true
