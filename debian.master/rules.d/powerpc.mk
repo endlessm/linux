4269e5fc5e49f53975f7a228fa19fe6b00136c1a
Tim Gardner <tim.gardner@canonical.com>
2014-07-24 07:28:11 -0600

    UBUNTU: Ubuntu-4.8.0-7.8
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

---
+++ debian.master/rules.d/powerpc.mk
@@ -0,0 +1,40 @@
+human_arch	= PowerPC (32 bit userspace)
+build_arch	= powerpc
+header_arch	= $(build_arch)
+defconfig	= pmac32_defconfig
+flavours	= powerpc-smp generic powerpc-e500mc powerpc64-emb
+build_image	= zImage
+kernel_file	= $(shell if [ ! -f $(builddir)/build-$*/vmlinux.strip ] && \
+		    [ -f $(builddir)/build-$*/vmlinux.strip.gz ]; then \
+			gunzip -c $(builddir)/build-$*/vmlinux.strip.gz \
+			> $(builddir)/build-$*/vmlinux.strip; \
+		    fi && echo vmlinux.strip)
+install_file	= vmlinux
+
+# These flavours differ
+build_image_powerpc-e500mc	= uImage
+kernel_file_powerpc-e500mc	= arch/powerpc/boot/uImage
+
+build_image_powerpc64-emb	= uImage
+kernel_file_powerpc64-emb	= arch/powerpc/boot/uImage
+
+loader		= yaboot
+vdso		= vdso_install
+
+custom_flavours	=
+
+no_dumpfile		= true
+do_tools_usbip          = true
+do_tools_cpupower	= true
+do_tools_perf		= true
+
+# Default to true and disable to ensure that the crossbuild
+# override still has priority.
+do_zfs			= true
+do_zfs_powerpc-smp	= false
+# do_zfs_generic	= true
+do_zfs_powerpc-e500mc	= false
+do_zfs_powerpc64-emb	= false
+do_extras_package	= true
+
+family			= ubuntu
