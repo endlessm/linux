1f3c0b5d35d2cf3bc9feba7ecc1eb0d84c34e8b1
Janusz Dziemidowicz <rraptorr@nails.eu.org>
2014-07-24 15:48:46 +0200

    scsi: do not issue SCSI RSOC command to Promise Vtrak E610f
    BugLink: http://bugs.launchpad.net/bugs/1371601
    
    commit 0213436a2cc5e4a5ca2fabfaa4d3877097f3b13f upstream.
    
    Some devices don't like REPORT SUPPORTED OPERATION CODES and will
    simply timeout causing sd_mod init to take a very very long time.
    Introduce BLIST_NO_RSOC scsi scan flag, that stops RSOC from being
    issued. Add it to Promise Vtrak E610f entry in scsi scan
    blacklist. Fixes bug #79901 reported at
    https://bugzilla.kernel.org/show_bug.cgi?id=79901
    
    Fixes: 98dcc2946adb ("SCSI: sd: Update WRITE SAME heuristics")
    
    Signed-off-by: Janusz Dziemidowicz <rraptorr@nails.eu.org>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- include/scsi/scsi_devinfo.h
+++ include/scsi/scsi_devinfo.h
@@ -33,4 +33,6 @@
 #define BLIST_NO_DIF		0x2000000 /* Disable T10 PI (DIF) */
 #define BLIST_SKIP_VPD_PAGES	0x4000000 /* Ignore SBC-3 VPD pages */
 #define BLIST_TRY_VPD_PAGES	0x10000000 /* Attempt to read VPD pages */
+#define BLIST_NO_RSOC		0x20000000 /* don't try to issue RSOC */
+
 #endif
