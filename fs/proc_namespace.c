9ca1fb67d57f9f6992d12232ce329cce4148a981
Rafael David Tinoco <rafael.tinoco@canonical.com>
2014-08-22 08:48:39 -0300

    namespaces: Use task_lock and not rcu to protect nsproxy
    BugLink: https://bugs.launchpad.net/bugs/1328088
    
    The synchronous syncrhonize_rcu in switch_task_namespaces makes setns
    a sufficiently expensive system call that people have complained.
    
    Upon inspect nsproxy no longer needs rcu protection for remote reads.
    remote reads are rare.  So optimize for same process reads and write
    by switching using rask_lock instead.
    
    This yields a simpler to understand lock, and a faster setns system call.
    
    In particular this fixes a performance regression observed
    by Rafael David Tinoco <rafael.tinoco@canonical.com>.
    
    This is effectively a revert of Pavel Emelyanov's commit
    cf7b708c8d1d7a27736771bcf4c457b332b0f818 Make access to task's nsproxy lighter
    from 2007.  The race this originialy fixed no longer exists as
    do_notify_parent uses task_active_pid_ns(parent) instead of
    parent->nsproxy.
    
    [Conflicts]
    
    ./fs/proc_namespace.c:
    
    * Upstream version patched if(!nsp || !nsp->mnt_ns) block
      but Trusty version is using 2 condition blocks to achieve
      same result. So rcu_unlock -> task_unlock substitution had
      to be done twice.
    
    OriginalAuthor: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    (back ported from commit 728dba3a39c66b3d8ac889ddbe38b5b1c264aec3 v3.17-rc1)
    Signed-off-by: "Rafael David Tinoco" <rafael.tinoco@canonical.com>
    Acked-by: Chris J Arges <chris.j.arges@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -232,22 +232,20 @@ static int mounts_open_common(struct inode *inode, struct file *file,
 	if (!task)
 		goto err;
 
-	rcu_read_lock();
-	nsp = task_nsproxy(task);
+	task_lock(task);
+	nsp = task->nsproxy;
 	if (!nsp) {
-		rcu_read_unlock();
+		task_unlock(task);
 		put_task_struct(task);
 		goto err;
 	}
 	ns = nsp->mnt_ns;
 	if (!ns) {
-		rcu_read_unlock();
+		task_unlock(task);
 		put_task_struct(task);
 		goto err;
 	}
 	get_mnt_ns(ns);
-	rcu_read_unlock();
-	task_lock(task);
 	if (!task->fs) {
 		task_unlock(task);
 		put_task_struct(task);
