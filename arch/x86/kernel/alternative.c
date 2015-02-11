2747a3a3b12bb5d3ef04c318174d67fa9ef50695
Arjan van de Ven <arjan@linux.intel.com>
2015-02-11 16:05:23 -0600

    UBUNTU: SAUCE: Clear Linux: bootstats: add printk's to measure boot time in more detail
    Few distro-tweaks to add printk's to visualize boot time better
    
    Author:    Arjan van de Ven <arjan@linux.intel.com>
    
    Signed-off-by: Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -626,7 +626,9 @@ void __init alternative_instructions(void)
 	 * patching.
 	 */
 
+	printk("Applying alternatives\n");
 	apply_alternatives(__alt_instructions, __alt_instructions_end);
+	printk("Applying alternatives done\n");
 
 #ifdef CONFIG_SMP
 	/* Patch to UP if other cpus not imminent. */
@@ -636,6 +638,8 @@ void __init alternative_instructions(void)
 					    __smp_locks, __smp_locks_end,
 					    _text, _etext);
 	}
+
+	printk("Applying alternatives smp done\n");
 
 	if (!uniproc_patched || num_possible_cpus() == 1)
 		free_init_pages("SMP alternatives",
