95acfa09efbfe968721250aa44d6358c6bb9e983
Carlo Caione <carlo@endlessm.com>
2015-03-27 20:42:14 +0100

    ALSA: hda - Add jack fixup for Quanta NL3
    The NL3 has both a headphone output jack and a headset jack, on
    the right edge of the chassis.
    
    The pin information suggests that both of these are at the Front.
    Pulseaudio currently can't deal with two different outputs with the
    same name (one having an index > 0).
    
    With this patch we retask the Jack as 'Dock Headphone Jack'. With this
    name change PA is able to recognize the Jack as a different Jack.
    
    Still the Jack is not functional since both the Jacks are actually
    mapped by PA on the same port and this doesn't work natively on the
    current version of PA. We fix this problem directly in PA so
    that both the Jacks are seen by PA as one single Jack and behave the
    same way (they are both mapped by PA to the same port).
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -4315,6 +4315,7 @@ enum {
 	ALC292_FIXUP_TPT440_DOCK,
 	ALC255_FIXUP_DELL_WMI_MIC_MUTE_LED,
 	ALC283_FIXUP_BXBT2807_MIC,
+	ALC269VC_FIXUP_NL3_SECOND_JACK,
 };
 
 static const struct hda_fixup alc269_fixups[] = {
@@ -4756,6 +4757,14 @@ static const struct hda_fixup alc269_fixups[] = {
 			{ },
 		},
 	},
+	[ALC269VC_FIXUP_NL3_SECOND_JACK] = {
+		.type = HDA_FIXUP_PINS,
+		.v.pins = (const struct hda_pintbl[]) {
+			{ 0x1a, 0x222140af },
+			{ },
+		},
+	},
+
 };
 
 static const struct snd_pci_quirk alc269_fixup_tbl[] = {
@@ -4890,6 +4899,7 @@ static const struct snd_pci_quirk alc269_fixup_tbl[] = {
 	SND_PCI_QUIRK(0x10cf, 0x1475, "Lifebook", ALC269_FIXUP_LIFEBOOK),
 	SND_PCI_QUIRK(0x10cf, 0x1845, "Lifebook U904", ALC269_FIXUP_LIFEBOOK_EXTMIC),
 	SND_PCI_QUIRK(0x1458, 0xfa53, "Gigabyte BXBT-2807", ALC283_FIXUP_BXBT2807_MIC),
+	SND_PCI_QUIRK(0x152d, 0x1082, "Quanta NL3", ALC269VC_FIXUP_NL3_SECOND_JACK),
 	SND_PCI_QUIRK(0x17aa, 0x20f2, "Thinkpad SL410/510", ALC269_FIXUP_SKU_IGNORE),
 	SND_PCI_QUIRK(0x17aa, 0x215e, "Thinkpad L512", ALC269_FIXUP_SKU_IGNORE),
 	SND_PCI_QUIRK(0x17aa, 0x21b8, "Thinkpad Edge 14", ALC269_FIXUP_SKU_IGNORE),
