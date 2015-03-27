eff8f8d78968a5f8df17542db7c837661bd36dc2
Carlo Caione <carlo@endlessm.com>
2015-03-27 20:42:14 +0100

    ALSA: hda - Add jack fixup for Quanta NL3
    The NL3 has both a headphone output jack and a headset jack, on
    the right edge of the chassis.
    
    The pin information suggests that both of these are at the Front.
    Pulseaudio currently can't deal with two different outputs with the
    same name (one having an index > 0).
    
    With this patch we retask the Jack as 'Dock Headphone Jack'. With this
    name change PA is able to recognize the Jack as a different Jack.
    
    Still the Jack is not functional since both the Jacks are actually
    mapped by PA on the same port and this doesn't work natively on the
    current version of PA. We fix this problem directly in PA so
    that both the Jacks are seen by PA as one single Jack and behave the
    same way (they are both mapped by PA to the same port).
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -5493,6 +5493,7 @@ enum {
 	ALC298_FIXUP_TPT470_DOCK,
 	ALC255_FIXUP_DUMMY_LINEOUT_VERB,
 	ALC255_FIXUP_DELL_HEADSET_MIC,
+	ALC269VC_FIXUP_NL3_SECOND_JACK,
 	ALC295_FIXUP_HP_X360,
 	ALC221_FIXUP_HP_HEADSET_MIC,
 };
@@ -6347,6 +6348,13 @@ static const struct hda_fixup alc269_fixups[] = {
 		.chained = true,
 		.chain_id = ALC269_FIXUP_HEADSET_MIC
 	},
+	[ALC269VC_FIXUP_NL3_SECOND_JACK] = {
+		.type = HDA_FIXUP_PINS,
+		.v.pins = (const struct hda_pintbl[]) {
+			{ 0x1a, 0x222140af },
+			{ },
+		},
+	},
 	[ALC295_FIXUP_HP_X360] = {
 		.type = HDA_FIXUP_FUNC,
 		.v.func = alc295_fixup_hp_top_speakers,
@@ -6531,6 +6539,7 @@ static const struct snd_pci_quirk alc269_fixup_tbl[] = {
 	SND_PCI_QUIRK(0x144d, 0xc740, "Samsung Ativ book 8 (NP870Z5G)", ALC269_FIXUP_ATIV_BOOK_8),
 	SND_PCI_QUIRK(0x1458, 0xfa53, "Gigabyte BXBT-2807", ALC283_FIXUP_HEADSET_MIC),
 	SND_PCI_QUIRK(0x1462, 0xb120, "MSI Cubi MS-B120", ALC283_FIXUP_HEADSET_MIC),
+	SND_PCI_QUIRK(0x152d, 0x1082, "Quanta NL3", ALC269VC_FIXUP_NL3_SECOND_JACK),
 	SND_PCI_QUIRK(0x17aa, 0x1036, "Lenovo P520", ALC233_FIXUP_LENOVO_MULTI_CODECS),
 	SND_PCI_QUIRK(0x17aa, 0x20f2, "Thinkpad SL410/510", ALC269_FIXUP_SKU_IGNORE),
 	SND_PCI_QUIRK(0x17aa, 0x215e, "Thinkpad L512", ALC269_FIXUP_SKU_IGNORE),
