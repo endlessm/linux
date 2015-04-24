9c8e344aa5b7186e2e74484c00fe6e51260c45f8
Carlo Caione <carlo@endlessm.com>
2015-04-24 14:38:58 +0200

    ALSA/hda: add new fixup for the automute on NL3 laptop
    To have both the audio jacks working at the same time on the NL3 laptop
    we need to force ALSA to set the 'Auto-Mute Mode' mixer control to
    'Speaker only'.  This patch adds a new fixup specific for the NL3 laptop
    to force this setting.
    
    Also includes a change from John Hiesey:
    
    The hda auto parser has a fixup that is enabled by default to
    try to detect when a supposed headphone jack is actually a
    line out jack. The change to set the other headphone jack to
    a 'Dock Headphone Jack' changed the sequence number of that
    jack to the correct value of 0xf; however, the front jack still
    gets an incorrect value from the hardware. This makes the auto
    parser fixup conclude the front jack is not really a headphone
    jack.
    
    Disable that erroneous fixup to allow more than one headphone
    jack to be detected without changing one to a line out jack.
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>
    
    [endlessm/eos-shell#5358]

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -5529,6 +5529,23 @@ static void alc_fixup_thinkpad_acpi(struct hda_codec *codec,
 /* for alc295_fixup_hp_top_speakers */
 #include "hp_x360_helper.c"
 
+static void alc269vc_nl3_fixup_automute(struct hda_codec *codec,
+					const struct hda_fixup *fix, int action)
+{
+	struct snd_kcontrol *kctl;
+	struct snd_ctl_elem_value *uctl;
+
+	kctl = snd_hda_find_mixer_ctl(codec, "Auto-Mute Mode");
+	if (!kctl)
+		return;
+	uctl = kzalloc(sizeof(*uctl), GFP_KERNEL);
+	if (!uctl)
+		return;
+	uctl->value.enumerated.item[0] = 1;
+	kctl->put(kctl, uctl);
+	kfree(uctl);
+}
+
 enum {
 	ALC269_FIXUP_SONY_VAIO,
 	ALC275_FIXUP_SONY_VAIO_GPIO2,
@@ -5651,6 +5668,7 @@ enum {
 	ALC255_FIXUP_DELL_HEADSET_MIC,
 	ALC256_FIXUP_HUAWEI_MBXP_PINS,
 	ALC269VC_FIXUP_NL3_SECOND_JACK,
+	ALC269VC_FIXUP_NL3_AUTOMUTE,
 	ALC295_FIXUP_HP_X360,
 	ALC221_FIXUP_HP_HEADSET_MIC,
 	ALC285_FIXUP_LENOVO_HEADPHONE_NOISE,
@@ -6546,6 +6564,14 @@ static const struct hda_fixup alc269_fixups[] = {
 			{ 0x1a, 0x222140af },
 			{ },
 		},
+		.chained = true,
+		.chain_id = ALC269VC_FIXUP_NL3_AUTOMUTE
+	},
+	[ALC269VC_FIXUP_NL3_AUTOMUTE] = {
+		.type = HDA_FIXUP_FUNC,
+		.v.func = alc269vc_nl3_fixup_automute,
+		.chained = true,
+		.chain_id = ALC269_FIXUP_PINCFG_NO_HP_TO_LINEOUT
 	},
 	[ALC295_FIXUP_HP_X360] = {
 		.type = HDA_FIXUP_FUNC,
