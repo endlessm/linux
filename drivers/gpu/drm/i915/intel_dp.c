b7c8d41e29cd535e4a8201022178b437d386a482
Sonika Jindal <sonika.jindal@intel.com>
2015-05-26 17:50:13 +0530

    drm/i915/bxt: edp1.4 Intermediate Freq support
    BugLink: http://bugs.launchpad.net/bugs/1494163
    
    BXT supports following intermediate link rates for edp:
    2.16GHz, 2.43GHz, 3.24GHz, 4.32GHz.
    Adding support for programming the intermediate rates.
    
    v2: Adding clock in bxt_clk_div struct and then look for the entry with
    required rate (Ville)
    v3: 'clock' has the selected value, no need to use link_bw or rate_select
    for selecting pll(Ville)
    v4: Make bxt_dp_clk_val const and remove size (Ville)
    v5: Rebased
    v6: Removed setting of vco while rebasing in v5, adding it back
    
    Signed-off-by: Sonika Jindal <sonika.jindal@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com> (v4)
    Reviewed-by: Vandana Kannan <vandana.kannan@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    (cherry picked from commit 64987fc59d90738715703362292f743b7dbbe76b)
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -91,6 +91,8 @@ static const struct dp_link_dpll chv_dpll[] = {
 		{ .p1 = 2, .p2 = 1, .n = 1, .m1 = 2, .m2 = 0x6c00000 } }
 };
 
+static const int bxt_rates[] = { 162000, 216000, 243000, 270000,
+				  324000, 432000, 540000 };
 static const int skl_rates[] = { 162000, 216000, 270000,
 				  324000, 432000, 540000 };
 static const int default_rates[] = { 162000, 270000, 540000 };
@@ -1209,7 +1211,10 @@ static bool intel_dp_source_supports_hbr2(struct drm_device *dev)
 static int
 intel_dp_source_rates(struct drm_device *dev, const int **source_rates)
 {
-	if (IS_SKYLAKE(dev)) {
+	if (IS_BROXTON(dev)) {
+		*source_rates = bxt_rates;
+		return ARRAY_SIZE(bxt_rates);
+	} else if (IS_SKYLAKE(dev)) {
 		*source_rates = skl_rates;
 		return ARRAY_SIZE(skl_rates);
 	}
