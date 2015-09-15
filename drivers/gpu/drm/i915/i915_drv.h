5b0366551f26e5b508987090c436f21e6d63e9b1
João Paulo Rechi Vita <jprvita@endlessm.com>
2015-09-15 14:52:44 -0400

    Revert "Revert "drm/i915: Use the disable callback for disabling planes.""
    This reverts commit 93766aa09f053d09b508df0fa1aabeff9a67438b.
    
    The original revert fixed the problem tracked by endlessm/eos-shell#5698
    (No boot splash after upgrade to Linux 4.2) but it introduced another
    problem: Fix laptop display after disconnect HDMI display (#5740).
    
    Since a proper fix has been provided by upstream to #5698, we can now
    revert the original revert.
    
    Signed-off-by: João Paulo Rechi Vita <jprvita@endlessm.com>
    
    [endlessm/eos-shell#5740]

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -273,6 +273,11 @@ struct i915_hotplug {
 #define for_each_crtc(dev, crtc) \
 	list_for_each_entry(crtc, &dev->mode_config.crtc_list, head)
 
+#define for_each_intel_plane(dev, intel_plane) \
+	list_for_each_entry(intel_plane,			\
+			    &dev->mode_config.plane_list,	\
+			    base.head)
+
 #define for_each_intel_crtc(dev, intel_crtc) \
 	list_for_each_entry(intel_crtc, &dev->mode_config.crtc_list, base.head)
 
