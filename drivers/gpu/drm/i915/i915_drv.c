f8a50149230be4b8903a659e81b984e1fde2f3e6
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2015-11-06 14:11:16 +0200

    drm/i915/skl: Add SKL GT4 PCI IDs
    BugLink: http://bugs.launchpad.net/bugs/1527462
    
    Add Skylake Intel Graphics GT4 PCI IDs
    
    v2: Rebase
    
    Signed-off-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Reviewed-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1446811876-303-1-git-send-email-mika.kuoppala@intel.com
    (back ported from commit 15620206ae87ba9643ffa6f5ddb5471be7192006)
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    
    Conflicts:
    	drivers/gpu/drm/i915/i915_drv.c

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -434,6 +434,7 @@ static const struct intel_device_info intel_broxton_info = {
 	INTEL_SKL_GT1_IDS(&intel_skylake_info),	\
 	INTEL_SKL_GT2_IDS(&intel_skylake_info),	\
 	INTEL_SKL_GT3_IDS(&intel_skylake_gt3_info),	\
+	INTEL_SKL_GT4_IDS(&intel_skylake_gt3_info),	\
 	INTEL_BXT_IDS(&intel_broxton_info)
 
 static const struct pci_device_id pciidlist[] = {		/* aka */
