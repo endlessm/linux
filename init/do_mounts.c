c38646a3898b91a35ff8900312218fd694ec5a37
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2015-11-20 14:01:26 -0600

    UBUNTU: SAUCE: Clear Linux: init: do_mounts: recreate /dev/root
    Rootfs shows as is mounted in /dev/root, but this devices is not present in
    /dev directory.
    
    Signed-off-by: Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -568,6 +568,8 @@ void __init mount_root(void)
  */
 void __init prepare_namespace(void)
 {
+	int err;
+
 	if (root_delay) {
 		printk(KERN_INFO "Waiting %d sec before mounting root device...\n",
 		       root_delay);
@@ -615,6 +617,13 @@ out:
 	devtmpfs_mount();
 	init_mount(".", "/", NULL, MS_MOVE, NULL);
 	init_chroot(".");
+#ifdef CONFIG_BLOCK
+	/* recreate the /dev/root */
+	err = create_dev("/dev/root", ROOT_DEV);
+
+	if (err < 0)
+		pr_emerg("Failed to create /dev/root: %d\n", err);
+#endif
 }
 
 static bool is_tmpfs;
