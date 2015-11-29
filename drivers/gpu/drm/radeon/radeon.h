87838fcc82f2275546f29dcbd6de42f38275b733
Julia Lawall <Julia.Lawall@lip6.fr>
2015-11-29 17:12:41 +0100

    drm/radeon: constify radeon_asic_ring structures
    BugLink: http://bugs.launchpad.net/bugs/1546572
    
    The radeon_asic_ring structures are never modified, so declare them as
    const.
    
    Done with the help of Coccinelle.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Julia Lawall <Julia.Lawall@lip6.fr>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit d26678da532401bd9227c8d79e902e59439b0140)
    Signed-off-by: Alberto Milone <alberto.milone@canonical.com>
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/gpu/drm/radeon/radeon.h
+++ drivers/gpu/drm/radeon/radeon.h
@@ -1890,7 +1890,7 @@ struct radeon_asic {
 		void (*pad_ib)(struct radeon_ib *ib);
 	} vm;
 	/* ring specific callbacks */
-	struct radeon_asic_ring *ring[RADEON_NUM_RINGS];
+	const struct radeon_asic_ring *ring[RADEON_NUM_RINGS];
 	/* irqs */
 	struct {
 		int (*set)(struct radeon_device *rdev);
