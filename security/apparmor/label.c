82c55fe3971c8f9b8a653632da931ebdb5ad37b5
John Johansen <john.johansen@canonical.com>
2015-12-15 04:43:30 -0800

    UBUNTU: SAUCE: fixup: make __share_replacedby private to get rid of build warning
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- security/apparmor/label.c
+++ security/apparmor/label.c
@@ -87,7 +87,7 @@ void __aa_update_replacedby(struct aa_label *orig, struct aa_label *new)
 	aa_put_label(tmp);
 }
 
-void __share_replacedby(struct aa_label *old, struct aa_label *new)
+static void __share_replacedby(struct aa_label *old, struct aa_label *new)
 {
 	struct aa_replacedby *r = new->replacedby;
 	new->replacedby = aa_get_replacedby(old->replacedby);
