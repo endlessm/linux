bd8cd68b7b1471ace5a86fb55ca9e2604e09e8fd
Serge Hallyn <serge.hallyn@ubuntu.com>
2016-01-05 20:12:21 +0000

    UBUNTU: SAUCE: add a sysctl to disable unprivileged user namespace unsharing
    It is turned on by default, but can be turned off if admins prefer or,
    more importantly, if a security vulnerability is found.
    
    The intent is to use this as mitigation so long as Ubuntu is on the
    cutting edge of enablement for things like unprivileged filesystem
    mounting.
    
    (This patch is tweaked from the one currently still in Debian sid, which
    in turn came from the patch we had in saucy)
    
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>
    [bwh: Remove unneeded binary sysctl bits]
    [ saf: move extern unprivileged_userns_clone declaration to
      include/linux/user_namespace.h to conform with 2374c09b1c8a
      "sysctl: remove all extern declaration from sysctl.c" ]
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -63,6 +63,7 @@
 #include <linux/mount.h>
 #include <linux/userfaultfd_k.h>
 #include <linux/pid.h>
+#include <linux/user_namespace.h>
 
 #include "../lib/kstrtox.h"
 
@@ -1764,6 +1765,15 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof (int),
 		.mode		= 0644,
 		.proc_handler	= sysrq_sysctl_handler,
+	},
+#endif
+#ifdef CONFIG_USER_NS
+	{
+		.procname	= "unprivileged_userns_clone",
+		.data		= &unprivileged_userns_clone,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
 	},
 #endif
 #ifdef CONFIG_PROC_SYSCTL
