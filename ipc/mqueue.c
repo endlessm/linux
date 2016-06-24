f256722836b0830b2923387e9b72f8eb10c3fcc4
Seth Forshee <seth.forshee@canonical.com>
2016-06-24 08:42:37 -0500

    Revert "UBUNTU: SAUCE: (namespace) mqueue: Super blocks must be owned by the user ns which owns the ipc ns"
    BugLink: http://bugs.launchpad.net/bugs/1588056
    
    This reverts commit dec77184fe7e43a3a505125b627eb245f8e12ce0.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- ipc/mqueue.c
+++ ipc/mqueue.c
@@ -326,10 +326,8 @@ static struct dentry *mqueue_mount(struct file_system_type *fs_type,
 			 int flags, const char *dev_name,
 			 void *data)
 {
-	struct ipc_namespace *ns = data;
-
 	if (!(flags & MS_KERNMOUNT)) {
-		ns = current->nsproxy->ipc_ns;
+		struct ipc_namespace *ns = current->nsproxy->ipc_ns;
 		/* Don't allow mounting unless the caller has CAP_SYS_ADMIN
 		 * over the ipc namespace.
 		 */
@@ -338,8 +336,7 @@ static struct dentry *mqueue_mount(struct file_system_type *fs_type,
 
 		data = ns;
 	}
-	return mount_ns_userns(fs_type, flags, ns->user_ns, data,
-			       mqueue_fill_super);
+	return mount_ns(fs_type, flags, data, mqueue_fill_super);
 }
 
 static void init_once(void *foo)
