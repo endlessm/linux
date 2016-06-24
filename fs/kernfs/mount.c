3988fb703506b2ae2deeee501d00f0afb18dc848
Seth Forshee <seth.forshee@canonical.com>
2016-06-24 08:42:57 -0500

    Revert "UBUNTU: SAUCE: kernfs: Do not match superblock in another user namespace when mounting"
    BugLink: http://bugs.launchpad.net/bugs/1588056
    
    This reverts commit aadbec3a89fe98b072e506e9af782b4485c642d8.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- fs/kernfs/mount.c
+++ fs/kernfs/mount.c
@@ -186,8 +186,7 @@ static int kernfs_test_super(struct super_block *sb, void *data)
 	struct kernfs_super_info *sb_info = kernfs_info(sb);
 	struct kernfs_super_info *info = data;
 
-	return sb_info->root == info->root && sb_info->ns == info->ns &&
-	       sb->s_user_ns == current_user_ns();
+	return sb_info->root == info->root && sb_info->ns == info->ns;
 }
 
 static int kernfs_set_super(struct super_block *sb, void *data)
