5e20a3b3095b891c84a119fca630e5db7dab3a79
João Paulo Rechi Vita <jprvita@endlessm.com>
2016-06-29 12:41:05 -0300

    x86: Lock down IO port read access if module signing is enforced
    This change comes from Matthew Garrett's securelevel patchset, which
    lock's IO port reading in addition to writing. But here we're using
    secure_modules() instead of securelevel.
    
    Signed-off-by: João Paulo Rechi Vita <jprvita@endlessm.com>

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -569,6 +569,9 @@ static ssize_t read_port(struct file *file, char __user *buf,
 	unsigned long i = *ppos;
 	char __user *tmp = buf;
 
+	if (secure_modules())
+		return -EPERM;
+
 	if (!access_ok(VERIFY_WRITE, buf, count))
 		return -EFAULT;
 	while (count-- > 0 && i < 65536) {
