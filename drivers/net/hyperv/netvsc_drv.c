bb844d1cd9cd6631ebe3f82e1dcf22e75c770e12
Stephen Hemminger <sthemmin@microsoft.com>
2016-09-22 16:56:30 -0700

    hv_netvsc: dev hold/put reference to VF
    BugLink: http://bugs.launchpad.net/bugs/1650059
    
    The netvsc driver holds a pointer to the virtual function network device if
    managing SR-IOV association. In order to ensure that the VF network device
    does not disappear, it should be using dev_hold/dev_put to get a reference
    count.
    
    Signed-off-by: Stephen Hemminger <sthemmin@microsoft.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 07d0f0008c783d2a2fce8497000938db15fd7aa1)
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Acked-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: Luis Henriques <luis.henriques@canonical.com>

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -1265,6 +1265,8 @@ static int netvsc_register_vf(struct net_device *vf_netdev)
 	 * Take a reference on the module.
 	 */
 	try_module_get(THIS_MODULE);
+
+	dev_hold(vf_netdev);
 	net_device_ctx->vf_netdev = vf_netdev;
 	return NOTIFY_OK;
 }
@@ -1379,6 +1381,7 @@ static int netvsc_unregister_vf(struct net_device *vf_netdev)
 	netdev_info(ndev, "VF unregistering: %s\n", vf_netdev->name);
 	netvsc_inject_disable(net_device_ctx);
 	net_device_ctx->vf_netdev = NULL;
+	dev_put(vf_netdev);
 	module_put(THIS_MODULE);
 	return NOTIFY_OK;
 }
