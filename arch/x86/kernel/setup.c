6bae9a8fa427f659411eec2eb36bf784ac62d020
David Howells <dhowells@redhat.com>
2016-11-21 23:55:55 +0000

    UBUNTU: SAUCE: (efi-lockdown) efi: Lock down the kernel if booted in secure boot mode
    UEFI Secure Boot provides a mechanism for ensuring that the firmware will
    only load signed bootloaders and kernels.  Certain use cases may also
    require that all kernel modules also be signed.  Add a configuration option
    that to lock down the kernel - which includes requiring validly signed
    modules - if the kernel is secure-booted.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    (cherry picked from commit 7b61978552b73cdf7c1e4bee993a4d2f4f896989
     git://git.kernel.org/pub/scm/linux/kernel/git/jwboyer/fedora.git)
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -70,6 +70,7 @@
 #include <linux/tboot.h>
 #include <linux/jiffies.h>
 #include <linux/mem_encrypt.h>
+#include <linux/security.h>
 
 #include <linux/usb/xhci-dbgp.h>
 #include <video/edid.h>
@@ -1204,7 +1205,12 @@ void __init setup_arch(char **cmdline_p)
 			break;
 		case efi_secureboot_mode_enabled:
 			set_bit(EFI_SECURE_BOOT, &efi.flags);
-			pr_info("Secure boot enabled\n");
+			if (IS_ENABLED(CONFIG_EFI_SECURE_BOOT_LOCK_DOWN)) {
+				lock_kernel_down();
+				pr_info("Secure boot enabled and kernel locked down\n");
+			} else {
+				pr_info("Secure boot enabled\n");
+			}
 			break;
 		default:
 			pr_info("Secure boot could not be determined\n");
