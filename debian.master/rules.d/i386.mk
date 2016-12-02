cb4a4e6e217a5e293682c8c539a7f833c5971e68
Tim Gardner <tim.gardner@canonical.com>
2016-12-01 20:51:06 -0700

    UBUNTU: Set build_arch=x86 for i386
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian.master/rules.d/i386.mk
+++ debian.master/rules.d/i386.mk
@@ -1,6 +1,6 @@
 human_arch	= 32 bit x86
-build_arch	= i386
-header_arch	= x86_64
+build_arch	= x86
+header_arch	= $(build_arch)
 defconfig	= defconfig
 flavours        = generic lowlatency
 build_image	= bzImage
