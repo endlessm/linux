295ffafba12793877fd5d9efd5a47e2a7a2e6435
Daniel Drake <drake@endlessm.com>
2016-12-08 08:47:29 -0600

    i8042: decrease more error messages to info
    Extend the Ubuntu/Intel commit titled
    "Clear Linux: i8042: decrease debug message level to info"
    to demote another message to info level, this one appears during
    boot on many systems without PS/2 ports like the ECS EF20.

--- drivers/input/serio/i8042.c
+++ drivers/input/serio/i8042.c
@@ -982,7 +982,7 @@ static int i8042_controller_init(void)
 			udelay(50);
 
 		if (i8042_command(&ctr[n++ % 2], I8042_CMD_CTL_RCTR)) {
-			pr_err("Can't read CTR while initializing i8042\n");
+			pr_info("Can't read CTR while initializing i8042\n");
 			return -EIO;
 		}
 
@@ -1033,7 +1033,7 @@ static int i8042_controller_init(void)
  */
 
 	if (i8042_command(&i8042_ctr, I8042_CMD_CTL_WCTR)) {
-		pr_err("Can't write CTR while initializing i8042\n");
+		pr_info("Can't write CTR while initializing i8042\n");
 		return -EIO;
 	}
 
