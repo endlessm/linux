f658ef27ff08ccd78c3418945a99daaec58ecca0
Hanjun Guo <hanjun.guo@linaro.org>
2017-01-18 20:55:00 +0800

    UBUNTU: SAUCE: ACPI: platform-msi: retrieve dev id from IORT
    For devices connecting to ITS, it needs dev id to identify itself, and
    this dev id is represented in the IORT table in named component node
    [1] for platform devices, so in this patch we will scan the IORT to
    retrieve device's dev id.
    
    With the preparation of iort_node_map_platform_id(), a new API
    iort_pmsi_get_dev_id() is introduced for that purpose, call it in
    its_pmsi_prepare() to make retrieving dev id ACPI aware.
    
    [1]: https://static.docs.arm.com/den0049/b/DEN0049B_IO_Remapping_Table.pdf
    
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>
    Tested-by: Ming Lei <ming.lei@canonical.com>
    Tested-by: Wei Xu <xuwei5@hisilicon.com>
    Tested-by: Sinan Kaya <okaya@codeaurora.org>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Cc: Tomasz Nowicki <tn@semihalf.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    [v8 submission from:
     https://www.spinics.net/lists/arm-kernel/msg555514.html ]
    BugLink: http://bugs.launchpad.net/bugs/1669061
    Signed-off-by: dann frazier <dann.frazier@canonical.com>
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- drivers/irqchip/irq-gic-v3-its-platform-msi.c
+++ drivers/irqchip/irq-gic-v3-its-platform-msi.c
@@ -57,7 +57,8 @@ static int its_pmsi_prepare(struct irq_domain *domain, struct device *dev,
 
 	msi_info = msi_get_domain_info(domain->parent);
 
-	ret = of_pmsi_get_dev_id(domain, dev, &dev_id);
+	ret = dev->of_node ? of_pmsi_get_dev_id(domain, dev, &dev_id) :
+		iort_pmsi_get_dev_id(dev, &dev_id);
 	if (ret)
 		return ret;
 
