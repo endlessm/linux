f474607989d981ea541fa71c9601f021f8b296e1
Daniel Drake <drake@endlessm.com>
2017-02-07 07:58:51 -0600

    i915: quirk Weibu F3C DP-to-VGA setup
    The Weibu F3C has a VGA port, enabled through an onboard PS8613
    DP-to-VGA converter chip.
    
    However the BIOS describes the DP interface from the SoC as being
    hooked up to an internal 1366x768 LCD panel over eDP, with a backlight.
    
    Until we get a fixed BIOS, force it to be detected as DP. That way
    we ignore the panel definition, don't register a backlight device,
    and we request the EDID over the AUX pins in order to get display modes.
    
    We also have a problem with presence detection and hotplug. The SoC
    detects the DP connection as always connected (since the chip is always
    there). The PS8613 does have a DP_HPD pin that looks like it could tell
    us when the VGA monitor is present and hotplugged, but we don't know how
    that is hooked up to the SoC.
    
    In order to work around this limitation, we poll the status every 10
    seconds. We force a re-read of the EDID even when there is no connector
    state change, and we update the connector status to disconnected if we
    fail to read the EDID.
    
    https://phabricator.endlessm.com/T14620

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -27,6 +27,7 @@
  *
  */
 
+#include <linux/dmi.h>
 #include <linux/acpi.h>
 #include <linux/device.h>
 #include <linux/oom.h>
@@ -1318,6 +1319,24 @@ static void i915_welcome_messages(struct drm_i915_private *dev_priv)
 		DRM_INFO("DRM_I915_DEBUG_GEM enabled\n");
 }
 
+static const struct dmi_system_id weibu_f3c[] = {
+	{
+		.ident = "Weibu F3C",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Weibu"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "F3C"),
+		},
+	},
+	{
+		.ident = "Endless EE-200",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Endless"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "EE-200"),
+		},
+	},
+	{ }
+};
+
 /**
  * i915_driver_load - setup chip and create an initial config
  * @pdev: PCI device
@@ -1351,6 +1370,7 @@ int i915_driver_load(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	dev_priv->drm.pdev = pdev;
 	dev_priv->drm.dev_private = dev_priv;
+	dev_priv->quirk_weibu_f3c = !!dmi_check_system(weibu_f3c);
 
 	ret = pci_enable_device(pdev);
 	if (ret)
