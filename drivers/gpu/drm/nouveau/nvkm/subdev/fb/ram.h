3e7b2af7e0525d096097af06416661fe8695304f
Ben Skeggs <bskeggs@redhat.com>
2017-03-03 08:44:01 +1000

    drm/nouveau/fb/gm200: split ram implementation from gm107
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/fb/ram.h
+++ drivers/gpu/drm/nouveau/nvkm/subdev/fb/ram.h
@@ -59,5 +59,6 @@ int mcp77_ram_new(struct nvkm_fb *, struct nvkm_ram **);
 int gf100_ram_new(struct nvkm_fb *, struct nvkm_ram **);
 int gk104_ram_new(struct nvkm_fb *, struct nvkm_ram **);
 int gm107_ram_new(struct nvkm_fb *, struct nvkm_ram **);
+int gm200_ram_new(struct nvkm_fb *, struct nvkm_ram **);
 int gp100_ram_new(struct nvkm_fb *, struct nvkm_ram **);
 #endif
