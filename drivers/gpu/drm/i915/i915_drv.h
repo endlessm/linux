e02e21fe23062c1683a716c7e33e73e8110eac30
Shashank Sharma <shashank.sharma@intel.com>
2017-03-28 17:57:51 +0800

    drm/i915: Parse VBT data for lspcon
    BugLink: https://bugs.launchpad.net/bugs/1676747
    
    Many GEN9 boards come with on-board lspcon cards.
    Fot these boards, VBT configuration should properly point out
    if a particular port contains lspcon device, so that driver can
    initialize it properly.
    
    This patch adds a utility function, which checks the VBT flag
    for lspcon bit, and tells us if a port is configured to have a
    lspcon device or not.
    
    V2: Fixed review comments from Ville
    - Do not forget PORT_D while checking lspcon for GEN9
    
    V3: Addressed review comments from Rodrigo
    - Create a HAS_LSPCON() macro for better use case handling.
    - Do not dump warnings for non-gen-9 platforms, it will be noise.
    
    V4: Rebase
    V5: Rebase
    V6: Pass dev_priv to HAS_LSPCON() macro
    
    Signed-off-by: Shashank Sharma <shashank.sharma@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Acked-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1476455212-27893-4-git-send-email-shashank.sharma@intel.com
    (cherry picked from commit 6389dd83768485a251dc0c5252dbc9a96f86ca53)
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2917,6 +2917,8 @@ struct drm_i915_cmd_table {
 #define HAS_GMCH_DISPLAY(dev) (INTEL_INFO(dev)->gen < 5 || \
 			       IS_VALLEYVIEW(dev) || IS_CHERRYVIEW(dev))
 
+#define HAS_LSPCON(dev_priv) (IS_GEN9(dev_priv))
+
 /* DPF == dynamic parity feature */
 #define HAS_L3_DPF(dev) (IS_IVYBRIDGE(dev) || IS_HASWELL(dev))
 #define NUM_L3_SLICES(dev) (IS_HSW_GT3(dev) ? 2 : HAS_L3_DPF(dev))
@@ -3739,6 +3741,9 @@ bool intel_bios_is_port_dp_dual_mode(struct drm_i915_private *dev_priv, enum por
 bool intel_bios_is_dsi_present(struct drm_i915_private *dev_priv, enum port *port);
 bool intel_bios_is_port_hpd_inverted(struct drm_i915_private *dev_priv,
 				     enum port port);
+bool intel_bios_is_lspcon_present(struct drm_i915_private *dev_priv,
+				enum port port);
+
 
 /* intel_opregion.c */
 #ifdef CONFIG_ACPI
