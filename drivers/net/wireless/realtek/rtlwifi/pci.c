f9cbd7e6b8226102714289bdce5488d150999cc2
Carlo Caione <carlo@endlessm.com>
2017-04-07 13:14:21 +0200

    RTL8723BE: Fix antenna selection for HP laptops
    The number of antennas connected to the WiFi/BT transceiver and their
    actual connector is an information carried inside the on-board EEPROM.
    When this information is wrong because the EEPROM is incorrectly
    programmed, the WiFi/BT signal is really weak and a stable connection is
    practically impossible.
    
    The EEPROM on the HP laptop correctly indicates that 1 single antenna
    is present (ant_num == ANT_X1 == 1) but the driver by default assumes
    that this is connected to BTC_ANTENNA_AT_MAIN_PORT. In our case (found
    experimentally) the single antenna is connected instead to
    BTC_ANTENNA_AT_AUX_PORT.
    
    Acting on the 'ant_num' module parameter we can discard the information
    carried by the EEPROM and force the driver to consider the single
    antenna connected to the BTC_ANTENNA_AT_AUX_PORT (using 'ant_num == 1').
    
    Readings:
    https://patchwork.kernel.org/patch/8603231/
    https://patchwork.kernel.org/patch/8603241/
    
    We have seen two instances of the 240 G5 model, one with
    DMI vendor "HP" and another with "Hewlett-Packard". In order to apply
    the workaround in both cases, we do not filter by the DMI vendor string.
    
    https://phabricator.endlessm.com/T15935
    https://phabricator.endlessm.com/T15935
    https://phabricator.endlessm.com/T20141
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>

--- drivers/net/wireless/realtek/rtlwifi/pci.c
+++ drivers/net/wireless/realtek/rtlwifi/pci.c
@@ -33,6 +33,7 @@
 #include <linux/export.h>
 #include <linux/kmemleak.h>
 #include <linux/module.h>
+#include <linux/dmi.h>
 
 MODULE_AUTHOR("lizhaoming	<chaoming_li@realsil.com.cn>");
 MODULE_AUTHOR("Realtek WlanFAE	<wlanfae@realtek.com>");
@@ -2169,6 +2170,28 @@ static void platform_enable_dma64(struct pci_dev *pdev, bool dma64)
 	pci_write_config_byte(pdev, 0x719, value);
 }
 
+static const struct dmi_system_id force_ant_sel[] = {
+	{
+		.ident = "HP 240 G5 Notebook PC",
+		.matches = {
+			DMI_MATCH(DMI_PRODUCT_NAME, "HP 240 G5 Notebook PC"),
+		},
+	},
+	{
+		.ident = "HP 240 G6 Notebook PC",
+		.matches = {
+			DMI_MATCH(DMI_PRODUCT_NAME, "HP 240 G6 Notebook PC"),
+		},
+	},
+	{
+		.ident = "HP 250 G5 Notebook PC",
+		.matches = {
+			DMI_MATCH(DMI_PRODUCT_NAME, "HP 250 G5 Notebook PC"),
+		},
+	},
+	{ /* sentinel */ }
+};
+
 int rtl_pci_probe(struct pci_dev *pdev,
 		  const struct pci_device_id *id)
 {
@@ -2333,6 +2356,10 @@ int rtl_pci_probe(struct pci_dev *pdev,
 	rtlpci->irq_alloc = 1;
 
 	set_bit(RTL_STATUS_INTERFACE_START, &rtlpriv->status);
+
+	if (dmi_check_system(force_ant_sel))
+		rtlpriv->cfg->mod_params->ant_sel = 1;
+
 	return 0;
 
 fail3:
