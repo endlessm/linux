d0d3b00eba96039707ee2e29d34033e9c54074f3
Seth Forshee <seth.forshee@canonical.com>
2017-05-04 08:09:04 -0500

    UBUNTU: SAUCE: (efi-lockdown) efi: Sanitize boot_params in efi stub
    The efi stub will set the value of boot_params.secure_boot
    without first checking whether boot_params has been sanitized. If
    they have not, the value of secure_boot will be cleared later
    when boot_params is sanitized. This currently happens with grub
    as it currently does not clear the sentinel, and thus the kernel
    cannot determine the secure boot state.
    
    Since the efi stub is modifying a field in an area subject to
    sanitization, it must first sanitize boot_params if needed. Later
    sanitization by the decompressor will do nothing as the sentinel
    value will have been cleared.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- arch/x86/boot/compressed/eboot.c
+++ arch/x86/boot/compressed/eboot.c
@@ -12,6 +12,7 @@
 #include <asm/efi.h>
 #include <asm/setup.h>
 #include <asm/desc.h>
+#include <asm/bootparam_utils.h>
 
 #include "../string.h"
 #include "eboot.h"
@@ -1015,6 +1016,8 @@ struct boot_params *efi_main(struct efi_config *c,
 
 	/* Ask the firmware to clear memory if we don't have a clean shutdown */
 	enable_reset_attack_mitigation();
+
+	sanitize_boot_params(boot_params);
 
 	/*
 	 * If the boot loader gave us a value for secure_boot then we use that,
