77b1ac7d94c1e6efd5d34b39135c36a03b665c9d
Seth Forshee <seth.forshee@canonical.com>
2017-05-26 16:08:55 -0500

    UBUNTU: SAUCE: iptables/ip6tables: Ensure entries are fully copied to userspace
    BugLink: http://bugs.launchpad.net/bugs/1691752
    
    Changes to copy entries one by one result in not copying the
    matches and the target to userpace, since these are not included
    in the size of struct ipt_entry and struct ip6t_entry. This causes
    a regression in deleting rules. Partially revert those changes to
    fix this.
    
    Fixes: f77bc5b23fb1 ("iptables: use match, target and data copy_to_user helpers")
    Fixes: e47ddb2c4691 ("ip6tables: use match, target and data copy_to_user helpers")
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- net/ipv6/netfilter/ip6_tables.c
+++ net/ipv6/netfilter/ip6_tables.c
@@ -872,6 +872,10 @@ copy_entries_to_user(unsigned int total_size,
 		return PTR_ERR(counters);
 
 	loc_cpu_entry = private->entries;
+	if (copy_to_user(userptr, loc_cpu_entry, total_size) != 0) {
+		ret = -EFAULT;
+		goto free_counters;
+	}
 
 	/* FIXME: use iterator macros --RR */
 	/* ... then go back and fix counters and names */
@@ -881,10 +885,6 @@ copy_entries_to_user(unsigned int total_size,
 		const struct xt_entry_target *t;
 
 		e = (struct ip6t_entry *)(loc_cpu_entry + off);
-		if (copy_to_user(userptr + off, e, sizeof(*e))) {
-			ret = -EFAULT;
-			goto free_counters;
-		}
 		if (copy_to_user(userptr + off
 				 + offsetof(struct ip6t_entry, counters),
 				 &counters[num],
