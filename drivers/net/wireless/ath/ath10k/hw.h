99f31fc7e35c0703858ba91cf5730b292919c9fb
Chris Chiu <chiu@endlessm.com>
2017-07-12 17:02:52 +0800

    ath10k: initialize regulatory domain based on SMBIOS data
    According to the specification "Qualcomm Atheros WLAN SMBIOS 80-Y7674-28
     Rev.D", Acer follow it to add a OEM-specific field in SMBIOS to
    program the regulatory domain (country code) based on target shipment
    country. It basically stores the flag which toggle the 11ac capability,
    country code flag and country code ANSI name based on ISO3166-1 alpha2.
    
    This commit read and parse data from SMBIOS and change regulatory
    settings while initializing the regulatory domain (country code).
    
    https://phabricator.endlessm.com/T18076
    
    Signed-off-by: Chris Chiu <chiu@endlessm.com>

--- drivers/net/wireless/ath/ath10k/hw.h
+++ drivers/net/wireless/ath/ath10k/hw.h
@@ -454,6 +454,11 @@ struct ath10k_hw_params {
 
 	/* Number of bytes to be discarded for each FFT sample */
 	int spectral_bin_discard;
+
+	/* In some regdomain(country), ex. EGYPT, the 11ac is totally
+	 * prohibited. SMBIOS keep this flag for regulatory configuration.
+	 */
+	bool vht_disable;
 };
 
 struct htt_rx_desc;
