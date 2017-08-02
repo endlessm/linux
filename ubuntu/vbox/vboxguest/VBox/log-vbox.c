890180fe342f74cbc6791a264f7742106684ab10
Seth Forshee <seth.forshee@canonical.com>
2017-08-02 08:24:33 -0500

    UBUNTU: ubuntu: vbox -- update to 5.1.26-dfsg-1
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- ubuntu/vbox/vboxguest/VBox/log-vbox.c
+++ ubuntu/vbox/vboxguest/VBox/log-vbox.c
@@ -747,8 +747,7 @@ RTDECL(PRTLOGGER) RTLogDefaultInit(void)
         pLogger->fDestFlags |= RTLOGDEST_DEBUGGER;
 # endif
 # if defined(DEBUG_aleksey)  /* Guest ring-0 as well */
-        //RTLogGroupSettings(pLogger, "net_flt_drv.e.l.f.l3.l4.l5 +net_adp_drv.e.l.f.l3.l4.l5");
-        RTLogGroupSettings(pLogger, "net_flt_drv.e.l.f.l3.l4.l5.l6");
+        RTLogGroupSettings(pLogger, "net_flt_drv.e.l.f.l3.l4.l5 +net_adp_drv.e.l.f.l3.l4.l5.l6");
         RTLogFlags(pLogger, "enabled unbuffered");
         pLogger->fDestFlags |= RTLOGDEST_DEBUGGER | RTLOGDEST_STDOUT;
 # endif
