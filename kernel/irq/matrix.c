3c333bdbb41686524261645de811ef1f3b971412
Daniel Drake <drake@endlessm.com>
2017-08-14 11:40:23 +0100

    PCI MSI: allow alignment restrictions on vector allocation
    ath9k hardware claims to support up to 4 MSI vectors, and when run in
    that configuration, it would be allowed to modify the lower bits of the
    MSI Message Data when generating interrupts in order to signal which
    of the 4 vectors the interrupt is being raised on.
    
    Linux's PCI-MSI irqchip only supports a single MSI vector for each
    device, and it tells the device this, but the device appears to assume
    it is working with 4, as it will unset the lower 2 bits of Message Data
    presumably to indicate that it is an IRQ for the first of 4 possible
    vectors.
    
    Linux will then receive an interrupt on the wrong vector, so the
    ath9k interrupt handler will not be invoked.
    
    To work around this, introduce a mechanism where the vector assignment
    algorithm can be restricted to only a subset of available vector numbers
    based on a bitmap.
    
    As a user of this bitmap, introduce a pci_dev.align_msi_vector flag which
    can be used to state that MSI vector numbers must be aligned to a specific
    amount. If we 4-align the ath9k MSI vector then the lower bits will
    already be 0 and hence the device will not modify the Message Data away
    from its original value.
    
    Signed-off-by: Daniel Drake <drake@endlessm.com>
    
    https://phabricator.endlessm.com/T16988

--- kernel/irq/matrix.c
+++ kernel/irq/matrix.c
@@ -108,14 +108,17 @@ void irq_matrix_offline(struct irq_matrix *m)
 }
 
 static unsigned int matrix_alloc_area(struct irq_matrix *m, struct cpumap *cm,
-				      unsigned int num, bool managed)
+				      unsigned int num, bool managed, unsigned int align)
 {
 	unsigned int area, start = m->alloc_start;
 	unsigned int end = m->alloc_end;
 
+	if (align > 0)
+		align--;
+
 	bitmap_or(m->scratch_map, cm->managed_map, m->system_map, end);
 	bitmap_or(m->scratch_map, m->scratch_map, cm->alloc_map, end);
-	area = bitmap_find_next_zero_area(m->scratch_map, end, start, num, 0);
+	area = bitmap_find_next_zero_area(m->scratch_map, end, start, num, align);
 	if (area >= end)
 		return area;
 	if (managed)
@@ -207,7 +210,7 @@ void irq_matrix_assign_system(struct irq_matrix *m, unsigned int bit,
  * on all CPUs in @msk, but it's not guaranteed that the bits are at the
  * same offset on all CPUs
  */
-int irq_matrix_reserve_managed(struct irq_matrix *m, const struct cpumask *msk)
+int irq_matrix_reserve_managed(struct irq_matrix *m, const struct cpumask *msk, unsigned int align)
 {
 	unsigned int cpu, failed_cpu;
 
@@ -215,7 +218,7 @@ int irq_matrix_reserve_managed(struct irq_matrix *m, const struct cpumask *msk)
 		struct cpumap *cm = per_cpu_ptr(m->maps, cpu);
 		unsigned int bit;
 
-		bit = matrix_alloc_area(m, cm, 1, true);
+		bit = matrix_alloc_area(m, cm, 1, true, align);
 		if (bit >= m->alloc_end)
 			goto cleanup;
 		cm->managed++;
@@ -283,7 +286,7 @@ void irq_matrix_remove_managed(struct irq_matrix *m, const struct cpumask *msk)
  * @cpu:	On which CPU the interrupt should be allocated
  */
 int irq_matrix_alloc_managed(struct irq_matrix *m, const struct cpumask *msk,
-			     unsigned int *mapped_cpu)
+			     unsigned int *mapped_cpu, unsigned int align)
 {
 	unsigned int bit, cpu, end = m->alloc_end;
 	struct cpumap *cm;
@@ -297,9 +300,14 @@ int irq_matrix_alloc_managed(struct irq_matrix *m, const struct cpumask *msk,
 
 	cm = per_cpu_ptr(m->maps, cpu);
 	end = m->alloc_end;
+
+	if (align > 0)
+		align--;
+
 	/* Get managed bit which are not allocated */
 	bitmap_andnot(m->scratch_map, cm->managed_map, cm->alloc_map, end);
-	bit = find_first_bit(m->scratch_map, end);
+	bitmap_complement(m->scratch_map, m->scratch_map, end);
+	bit = bitmap_find_next_zero_area(m->scratch_map, end, 0, 1, align);
 	if (bit >= end)
 		return -ENOSPC;
 	set_bit(bit, cm->alloc_map);
@@ -375,7 +383,7 @@ void irq_matrix_remove_reserved(struct irq_matrix *m)
  * @mapped_cpu: Pointer to store the CPU for which the irq was allocated
  */
 int irq_matrix_alloc(struct irq_matrix *m, const struct cpumask *msk,
-		     bool reserved, unsigned int *mapped_cpu)
+		     bool reserved, unsigned int *mapped_cpu, unsigned int align)
 {
 	unsigned int cpu, bit;
 	struct cpumap *cm;
@@ -385,7 +393,7 @@ int irq_matrix_alloc(struct irq_matrix *m, const struct cpumask *msk,
 		return -ENOSPC;
 
 	cm = per_cpu_ptr(m->maps, cpu);
-	bit = matrix_alloc_area(m, cm, 1, false);
+	bit = matrix_alloc_area(m, cm, 1, false, align);
 	if (bit >= m->alloc_end)
 		return -ENOSPC;
 	cm->allocated++;
@@ -397,7 +405,6 @@ int irq_matrix_alloc(struct irq_matrix *m, const struct cpumask *msk,
 	*mapped_cpu = cpu;
 	trace_irq_matrix_alloc(bit, cpu, m, cm);
 	return bit;
-
 }
 
 /**
