94bdca35cf3d2d559238068048de2d0caa7d9f52
Alex Deucher <alexander.deucher@amd.com>
2017-08-17 16:41:13 -0400

    drm/amdgpu: fix a bogus warning
    Don't validate the default value.  Prevents a needless
    warning. Also fix a spelling typo in the warning message.
    
    Reviewed-by: Alex Xie <AlexBin.Xie@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -1078,8 +1078,9 @@ static void amdgpu_check_arguments(struct amdgpu_device *adev)
 	}
 
 	/* valid range is between 4 and 9 inclusive */
-	if (amdgpu_vm_fragment_size > 9 || amdgpu_vm_fragment_size < 4) {
-		dev_warn(adev->dev, "valid rang is between 4 and 9\n");
+	if (amdgpu_vm_fragment_size != -1 &&
+	    (amdgpu_vm_fragment_size > 9 || amdgpu_vm_fragment_size < 4)) {
+		dev_warn(adev->dev, "valid range is between 4 and 9\n");
 		amdgpu_vm_fragment_size = -1;
 	}
 
