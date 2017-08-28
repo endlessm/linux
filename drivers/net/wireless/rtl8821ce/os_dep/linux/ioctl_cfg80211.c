5fd4d9eb94867c3574bf09546ff29cd6b2e221f6
Daniel Drake <drake@endlessm.com>
2017-08-28 13:21:22 +0800

    rtl8821ce: integrate and fix build
    https://phabricator.endlessm.com/T18646

--- drivers/net/wireless/rtl8821ce/os_dep/linux/ioctl_cfg80211.c
+++ drivers/net/wireless/rtl8821ce/os_dep/linux/ioctl_cfg80211.c
@@ -686,15 +686,7 @@ static int rtw_cfg80211_sync_iftype(_adapter *adapter)
 
 static u64 rtw_get_systime_us(void)
 {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
-	struct timespec ts;
-	get_monotonic_boottime(&ts);
-	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
-#else
-	struct timeval tv;
-	do_gettimeofday(&tv);
-	return ((u64)tv.tv_sec * 1000000) + tv.tv_usec;
-#endif
+	return ktime_to_us(ktime_get_boottime());
 }
 
 /* Try to remove non target BSS's SR to reduce PBC overlap rate */
