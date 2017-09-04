98a9550398f87c5430d5e893104e21caa1e2e8d3
Elena Reshetova <elena.reshetova@intel.com>
2017-09-04 13:11:55 +0300

    net: mpls: prevent speculative execution
    CVE-2017-5753
    CVE-2017-5715
    
    Real commit text tbd
    
    Signed-off-by: Elena Reshetova <elena.reshetova@intel.com>
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- net/mpls/af_mpls.c
+++ net/mpls/af_mpls.c
@@ -50,6 +50,8 @@ static struct mpls_route *mpls_route_input_rcu(struct net *net, unsigned index)
 	if (index < net->mpls.platform_labels) {
 		struct mpls_route __rcu **platform_label =
 			rcu_dereference(net->mpls.platform_label);
+
+		gmb();
 		rt = rcu_dereference(platform_label[index]);
 	}
 	return rt;
