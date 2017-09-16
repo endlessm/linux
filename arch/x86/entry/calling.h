725ad2ef81ccceb3e31a7263faae2059d05e2c48
Tim Chen <tim.c.chen@linux.intel.com>
2017-09-15 19:41:24 -0700

    x86/syscall: Clear unused extra registers on 32-bit compatible syscall entrance
    CVE-2017-5753
    CVE-2017-5715
    
    To prevent the unused registers %r8-%r15, from being used speculatively,
    we clear them upon syscall entrance for code hygiene in 32 bit compatible
    mode.
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- arch/x86/entry/calling.h
+++ arch/x86/entry/calling.h
@@ -155,6 +155,17 @@ For 32-bit we have the following conventions - kernel is built with
 	popq %rbx
 	.endm
 
+	.macro CLEAR_R8_TO_R15
+	xorq %r15, %r15
+	xorq %r14, %r14
+	xorq %r13, %r13
+	xorq %r12, %r12
+	xorq %r11, %r11
+	xorq %r10, %r10
+	xorq %r9, %r9
+	xorq %r8, %r8
+	.endm
+
 	.macro CLEAR_EXTRA_REGS
 	xorq %r15, %r15
 	xorq %r14, %r14
