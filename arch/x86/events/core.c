73b4d4bc16f6d80128ea2e040e83d0f352f7bde2
Will Deacon <will.deacon@arm.com>
2017-10-24 11:22:48 +0100

    locking/barriers: Convert users of lockless_dereference() to READ_ONCE()
    commit 3382290ed2d5e275429cef510ab21889d3ccd164 upstream.
    
    [ Note, this is a Git cherry-pick of the following commit:
    
        506458efaf15 ("locking/barriers: Convert users of lockless_dereference() to READ_ONCE()")
    
      ... for easier x86 PTI code testing and back-porting. ]
    
    READ_ONCE() now has an implicit smp_read_barrier_depends() call, so it
    can be used instead of lockless_dereference() without any change in
    semantics.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1508840570-22169-4-git-send-email-will.deacon@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/events/core.c
+++ arch/x86/events/core.c
@@ -2336,7 +2336,7 @@ static unsigned long get_segment_base(unsigned int segment)
 		struct ldt_struct *ldt;
 
 		/* IRQs are off, so this synchronizes with smp_store_release */
-		ldt = lockless_dereference(current->active_mm->context.ldt);
+		ldt = READ_ONCE(current->active_mm->context.ldt);
 		if (!ldt || idx >= ldt->nr_entries)
 			return 0;
 
