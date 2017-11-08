6591b0fadd1a1fc601681bbc0014b9b2579dc8bf
Daniel Drake <drake@endlessm.com>
2017-11-08 10:04:00 +0800

    mac80211: verbose logging for WLAN_REASON_DISASSOC_DUE_TO_INACTIVITY
    We're seeing multiple reports of ath9k disconnecting with
    locally generated reason code 4.
    
    As we do not yet have usable debug logs for thie issue, add verbose logging
    so that we can more easily get more info on which codepath is being hit
    here.
    
    Revert this once the underlying issue is found and fixed.
    
    https://phabricator.endlessm.com/T19774

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -2408,6 +2408,7 @@ static void __ieee80211_disconnect(struct ieee80211_sub_if_data *sdata)
 	 */
 	cfg80211_unlink_bss(local->hw.wiphy, ifmgd->associated);
 
+	sdata_info(sdata, "__ieee80211_disconnect due to inactivity\n");
 	ieee80211_set_disassoc(sdata, IEEE80211_STYPE_DEAUTH,
 			       WLAN_REASON_DISASSOC_DUE_TO_INACTIVITY,
 			       true, frame_buf);
@@ -3966,7 +3967,7 @@ void ieee80211_sta_work(struct ieee80211_sub_if_data *sdata)
 					 max_tries);
 				ieee80211_mgd_probe_ap_send(sdata);
 			} else {
-				mlme_dbg(sdata,
+				sdata_info(sdata,
 					 "No ack for nullfunc frame to AP %pM, disconnecting.\n",
 					 bssid);
 				ieee80211_sta_connection_lost(sdata, bssid,
@@ -3976,7 +3977,7 @@ void ieee80211_sta_work(struct ieee80211_sub_if_data *sdata)
 		} else if (time_is_after_jiffies(ifmgd->probe_timeout))
 			run_again(sdata, ifmgd->probe_timeout);
 		else if (ieee80211_hw_check(&local->hw, REPORTS_TX_ACK_STATUS)) {
-			mlme_dbg(sdata,
+			sdata_info(sdata,
 				 "Failed to send nullfunc to AP %pM after %dms, disconnecting\n",
 				 bssid, probe_wait_ms);
 			ieee80211_sta_connection_lost(sdata, bssid,
@@ -3992,7 +3993,7 @@ void ieee80211_sta_work(struct ieee80211_sub_if_data *sdata)
 			 * We actually lost the connection ... or did we?
 			 * Let's make sure!
 			 */
-			mlme_dbg(sdata,
+			sdata_info(sdata,
 				 "No probe response from AP %pM after %dms, disconnecting.\n",
 				 bssid, probe_wait_ms);
 
