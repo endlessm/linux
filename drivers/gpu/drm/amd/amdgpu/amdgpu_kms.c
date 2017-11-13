00e95eccabc03408c71d6acd61fcd1e24b24f51f
Shirish S <shirish.s@amd.com>
2017-11-13 13:40:28 +0530

    FORKLIFT: amd: copy amd folder as is from dave airlie tree
    Below is the git # of the branch
    https://cgit.freedesktop.org/~airlied/linux/log/?h=drm-next-amd-dc-staging
    
    26c860d Merge branch 'drm-next-4.15-dc' of git://people.freedesktop.org/~agd5f/linux into drm-next
    1f3493f drm/amdgpu/display: fix integer arithmetic problem
    d8d46ae drm/amdgpu/display: remove unused REG_OFFSET macro
    d8eed82 drm/amdgpu/display: provide ASSERT macros unconditionally
    4a74635 drm/amd/display: Read resource_straps from registers for DCE12
    b7fa851 drm/amd: Add DCE12 resource strap registers
    96719c5 drm/amd/display: Explicitly call ->reset for each object
    cd8a2ae drm/amd/display: Use single fail label in init_drm_dev
    efa6a8b drm/amd/display: Use plane pointer to avoid line breaks
    55d9038 drm/amd/display: fix null pointer dereference
    f5ba60f amdgpu/dc: Avoid dereferencing NULL pointer
    30b7c61 drm/amd/display: Don't print error when bo_pin is interrupted
    cc57306 drm/amd/display: Use constants from atom.h for HDMI caps read
    7bef1af drm/amd/display: check if modeset is required before adding plane
    56087b31 drm/amd/display: fix high part address in dm_plane_helper_prepare_fb()
    4d3e00d drm/amd/display : add high part address calculation for underlay
    3b21b6d drm/amd/display: Fix no display on Fiji
    9a68db7 Revert "drm/amd/display: Match actual state during S3 resume."
    af91113 Merge branch 'drm-next-4.15-dc' of git://people.freedesktop.org/~agd5f/linux into drm-next
    9b38bd1 drm/amd/display:: Fix NULL pointer in Raven hotplug
    af09e48 drm/amd/display: Fix memoryleak during S3 resume.
    fc9e992 drm/amd/display: add hardware_planes_only to list of affected planes
    
    Change-Id: I2f2863394a462208f490bed257f17e68fd6cf1e0
    Signed-off-by: Shirish S <shirish.s@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
@@ -825,7 +825,7 @@ int amdgpu_driver_open_kms(struct drm_device *dev, struct drm_file *file_priv)
 	}
 
 	r = amdgpu_vm_init(adev, &fpriv->vm,
-			   AMDGPU_VM_CONTEXT_GFX);
+			   AMDGPU_VM_CONTEXT_GFX, 0);
 	if (r) {
 		kfree(fpriv);
 		goto out_suspend;
@@ -841,8 +841,11 @@ int amdgpu_driver_open_kms(struct drm_device *dev, struct drm_file *file_priv)
 
 	if (amdgpu_sriov_vf(adev)) {
 		r = amdgpu_map_static_csa(adev, &fpriv->vm, &fpriv->csa_va);
-		if (r)
+		if (r) {
+			amdgpu_vm_fini(adev, &fpriv->vm);
+			kfree(fpriv);
 			goto out_suspend;
+		}
 	}
 
 	mutex_init(&fpriv->bo_list_lock);
@@ -1021,6 +1024,7 @@ const struct drm_ioctl_desc amdgpu_ioctls_kms[] = {
 	DRM_IOCTL_DEF_DRV(AMDGPU_CTX, amdgpu_ctx_ioctl, DRM_AUTH|DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(AMDGPU_VM, amdgpu_vm_ioctl, DRM_AUTH|DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(AMDGPU_BO_LIST, amdgpu_bo_list_ioctl, DRM_AUTH|DRM_RENDER_ALLOW),
+	DRM_IOCTL_DEF_DRV(AMDGPU_FENCE_TO_HANDLE, amdgpu_cs_fence_to_handle_ioctl, DRM_AUTH|DRM_RENDER_ALLOW),
 	/* KMS */
 	DRM_IOCTL_DEF_DRV(AMDGPU_GEM_MMAP, amdgpu_gem_mmap_ioctl, DRM_AUTH|DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(AMDGPU_GEM_WAIT_IDLE, amdgpu_gem_wait_idle_ioctl, DRM_AUTH|DRM_RENDER_ALLOW),
@@ -1031,7 +1035,7 @@ const struct drm_ioctl_desc amdgpu_ioctls_kms[] = {
 	DRM_IOCTL_DEF_DRV(AMDGPU_GEM_METADATA, amdgpu_gem_metadata_ioctl, DRM_AUTH|DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(AMDGPU_GEM_VA, amdgpu_gem_va_ioctl, DRM_AUTH|DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(AMDGPU_GEM_OP, amdgpu_gem_op_ioctl, DRM_AUTH|DRM_RENDER_ALLOW),
-	DRM_IOCTL_DEF_DRV(AMDGPU_GEM_USERPTR, amdgpu_gem_userptr_ioctl, DRM_AUTH|DRM_RENDER_ALLOW),
+	DRM_IOCTL_DEF_DRV(AMDGPU_GEM_USERPTR, amdgpu_gem_userptr_ioctl, DRM_AUTH|DRM_RENDER_ALLOW)
 };
 const int amdgpu_max_kms_ioctl = ARRAY_SIZE(amdgpu_ioctls_kms);
 
