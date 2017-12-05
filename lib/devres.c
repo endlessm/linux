6c4aff2f384abe2b38a898895fd103571dc87d23
Daniel Drake <drake@endlessm.com>
2017-12-05 08:52:40 +0000

    devres ioremap: demote bootup errors to warnings
    This error has been reported as visible during boot, but we do
    not know of any user-visible side effects of this error condition.
    
    Demote it to a warning so that it does not appear on the console.
    This change is temporary; in EOS-3.4+ we have a more intelligent
    way of showing/hiding console messages.
    
    https://phabricator.endlessm.com/T20205

--- lib/devres.c
+++ lib/devres.c
@@ -148,7 +148,7 @@ void __iomem *devm_ioremap_resource(struct device *dev, struct resource *res)
 	name = res->name ?: dev_name(dev);
 
 	if (!devm_request_mem_region(dev, res->start, size, name)) {
-		dev_err(dev, "can't request region for resource %pR\n", res);
+		dev_warn(dev, "can't request region for resource %pR\n", res);
 		return IOMEM_ERR_PTR(-EBUSY);
 	}
 
