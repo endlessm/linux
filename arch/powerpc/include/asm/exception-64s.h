aa20eb30c7c809024f8be2298ad10c8e4d621229
Michael Ellerman <mpe@ellerman.id.au>
2018-01-05 17:53:23 +0530

    UBUNTU: SAUCE: rfi-flush: Make DEBUG_RFI a CONFIG option
    CVE-2017-5754
    
    BugLink: http://bugs.launchpad.net/bugs/1742772
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- arch/powerpc/include/asm/exception-64s.h
+++ arch/powerpc/include/asm/exception-64s.h
@@ -82,9 +82,7 @@
 	RFI_FLUSH_FIXUP_SECTION;					\
 	nop
 
-#define DEBUG_RFI
-
-#ifdef DEBUG_RFI
+#ifdef CONFIG_PPC_DEBUG_RFI
 #define CHECK_TARGET_MSR_PR(srr_reg, expected_pr)			\
 	SET_SCRATCH0(r3);						\
 	mfspr	r3,srr_reg;						\
