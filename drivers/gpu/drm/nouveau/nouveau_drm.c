ae4e18c75e304e59d2e86df686fc1704d3dff37e
Chris Chiu <chiu@endlessm.com>
2018-01-22 21:41:16 +0800

    drm/nouveau: Disable runtime pm on Asus Notebooks with NV GM108
    The Asus X542UF/X542UQ, P1440UF/P2440UF and more models with NVIDIA
    940MX, MX110/MX130 has a problem with the interaction between runtime
    pm and suspend/resume which leads to the machine hanging on boot or
    shutdown. This commit disables runtime pm for all Asus Notebooks with
    the display chip NV GM108.
    
    It has to be reverted once if there's a upstream fix.
    
    https://phabricator.endlessm.com/T20646
    
    Signed-off-by: Chris Chiu <chiu@endlessm.com>

--- drivers/gpu/drm/nouveau/nouveau_drm.c
+++ drivers/gpu/drm/nouveau/nouveau_drm.c
@@ -551,6 +551,17 @@ static const struct dmi_system_id gp107_runpm_blacklist[] = {
 	{ }
 };
 
+static const struct dmi_system_id gm108_runpm_blacklist[] = {
+	{
+                .ident = "ASUS laptop",
+                .matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_CHASSIS_TYPE, "10"), /* Notebook */
+                },
+        },
+	{ }
+};
+
 static int
 nouveau_drm_load(struct drm_device *dev, unsigned long flags)
 {
@@ -589,6 +600,10 @@ nouveau_drm_load(struct drm_device *dev, unsigned long flags)
 
 	if (drm->client.device.info.chipset == 0x137 &&
 	    dmi_check_system(gp107_runpm_blacklist))
+		nouveau_runtime_pm = 0;
+
+	if (drm->client.device.info.chipset == 0x118 &&
+	    dmi_check_system(gm108_runpm_blacklist))
 		nouveau_runtime_pm = 0;
 
 	nouveau_vga_init(drm);
