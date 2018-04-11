4648636f1ccc61090e3a6195180c81888e405400
Martin Schwidefsky <schwidefsky@de.ibm.com>
2018-04-11 08:35:23 +0200

    s390: correct nospec auto detection init order
    BugLink: http://bugs.launchpad.net/bugs/1762719
    
    With CONFIG_EXPOLINE_AUTO=y the call of spectre_v2_auto_early() via
    early_initcall is done *after* the early_param functions. This
    overwrites any settings done with the nobp/no_spectre_v2/spectre_v2
    parameters. The code patching for the kernel is done after the
    evaluation of the early parameters but before the early_initcall
    is done. The end result is a kernel image that is patched correctly
    but the kernel modules are not.
    
    Make sure that the nospec auto detection function is called before the
    early parameters are evaluated and before the code patching is done.
    
    Fixes: 6e179d64126b ("s390: add automatic detection of the spectre defense")
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    (cherry picked from commit 6a3d1e81a434fc311f224b8be77258bafc18ccc6
     git://git.kernel.org/pub/scm/linux/kernel/git/s390/linux.git)
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- arch/s390/kernel/setup.c
+++ arch/s390/kernel/setup.c
@@ -898,6 +898,9 @@ void __init setup_arch(char **cmdline_p)
 	if (IS_ENABLED(CONFIG_EXPOLINE_AUTO))
 		nospec_auto_detect();
 
+	if (IS_ENABLED(CONFIG_EXPOLINE_AUTO))
+		nospec_auto_detect();
+
 	parse_early_param();
 #ifdef CONFIG_CRASH_DUMP
 	/* Deactivate elfcorehdr= kernel parameter */
