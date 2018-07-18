29a46a97e7507da349a57d49c36cd4e330c79904
João Paulo Rechi Vita <jprvita@endlessm.com>
2018-07-18 11:32:08 -0700

    [Endless] packaging: Import linux-meta packages
    Import meta packages from linux-meta that map to the versioned packages
    built by the linux source package. This way we can get rid of linux-meta
    and don't need to bump its ABI version on every rebase.
    
    https://phabricator.endlessm.com/T16504

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -602,14 +602,20 @@ endif
 endif
 
 binary-%: pkgimg = $(bin_pkg_name)-$*
+binary-%: metapkgimg = linux-image-$*
+binary-%: metapkgsignedimg = linux-signed-image-$*
+binary-%: metapkgsigned = linux-signed-$*
+binary-%: metapkg = linux-$*
 binary-%: pkgimg_mods = $(mods_pkg_name)-$*
 binary-%: pkgimg_ex = $(mods_extra_pkg_name)-$*
 binary-%: pkgdir_ex = $(CURDIR)/debian/$(extra_pkg_name)-$*
 binary-%: pkgbldinfo = $(bldinfo_pkg_name)-$*
 binary-%: pkghdr = $(hdrs_pkg_name)-$*
+binary-%: metapkghdr = linux-headers-$*
 binary-%: dbgpkg = $(bin_pkg_name)-$*-dbgsym
 binary-%: dbgpkgdir = $(CURDIR)/debian/$(bin_pkg_name)-$*-dbgsym
 binary-%: pkgtools = $(tools_flavour_pkg_name)-$*
+binary-%: metapkgtools = linux-tools-$*
 binary-%: pkgcloud = $(cloud_flavour_pkg_name)-$*
 $(foreach _m,$(all_dkms_modules), \
   $(eval binary-%: enable_$(_m) = $$(filter true,$$(call custom_override,do_$(_m),$$*))) \
@@ -623,6 +629,53 @@ binary-%: checks-%
 
 	$(call dh_all,$(pkgimg)) -- -Znone
 	$(call dh_all,$(pkgimg_mods))
+
+	dh_installdirs -p$(metapkgimg)
+	dh_installdocs -p$(metapkgimg)
+	dh_installchangelogs -p$(metapkgimg)
+	dh_compress -p$(metapkgimg)
+	dh_fixperms -p$(metapkgimg)
+	dh_installdeb -p$(metapkgimg)
+	$(lockme) dh_gencontrol -p$(metapkgimg)
+	dh_md5sums -p$(metapkgimg)
+	dh_builddeb -p$(metapkgimg)
+	dh_installdirs -p$(metapkgsignedimg)
+	dh_installdocs -p$(metapkgsignedimg)
+	dh_installchangelogs -p$(metapkgsignedimg)
+	dh_compress -p$(metapkgsignedimg)
+	dh_fixperms -p$(metapkgsignedimg)
+	dh_installdeb -p$(metapkgsignedimg)
+	$(lockme) dh_gencontrol -p$(metapkgsignedimg)
+	dh_md5sums -p$(metapkgsignedimg)
+	dh_builddeb -p$(metapkgsignedimg)
+	dh_installdirs -p$(metapkgsigned)
+	dh_installdocs -p$(metapkgsigned)
+	dh_installchangelogs -p$(metapkgsigned)
+	dh_compress -p$(metapkgsigned)
+	dh_fixperms -p$(metapkgsigned)
+	dh_installdeb -p$(metapkgsigned)
+	$(lockme) dh_gencontrol -p$(metapkgsigned)
+	dh_md5sums -p$(metapkgsigned)
+	dh_builddeb -p$(metapkgsigned)
+	dh_installdirs -p$(metapkg)
+	dh_installdocs -p$(metapkg)
+	dh_installchangelogs -p$(metapkg)
+	dh_compress -p$(metapkg)
+	dh_fixperms -p$(metapkg)
+	dh_installdeb -p$(metapkg)
+	$(lockme) dh_gencontrol -p$(metapkg)
+	dh_md5sums -p$(metapkg)
+	dh_builddeb -p$(metapkg)
+
+	dh_installchangelogs -p$(pkgimg_mods)
+	dh_installdocs -p$(pkgimg_mods)
+	dh_compress -p$(pkgimg_mods)
+	dh_fixperms -p$(pkgimg_mods) -X/boot/
+	dh_installdeb -p$(pkgimg_mods)
+	dh_shlibdeps -p$(pkgimg_mods) $(shlibdeps_opts)
+	$(lockme) dh_gencontrol -p$(pkgimg_mods)
+	dh_md5sums -p$(pkgimg_mods)
+	dh_builddeb -p$(pkgimg_mods)
 
 ifeq ($(do_extras_package),true)
   ifeq ($(ship_extras_package),false)
@@ -645,6 +698,15 @@ endif
 
 	$(call dh_all,$(pkgbldinfo))
 	$(call dh_all,$(pkghdr))
+	dh_installdirs -p$(metapkghdr)
+	dh_installdocs -p$(metapkghdr)
+	dh_installchangelogs -p$(metapkghdr)
+	dh_compress -p$(metapkghdr)
+	dh_fixperms -p$(metapkghdr)
+	dh_installdeb -p$(metapkghdr)
+	$(lockme) dh_gencontrol -p$(metapkghdr)
+	dh_md5sums -p$(metapkghdr)
+	dh_builddeb -p$(metapkghdr)
 
 ifneq ($(skipsub),true)
 	@set -e; for sub in $($(*)_sub); do		\
@@ -680,6 +742,15 @@ endif
 
 ifeq ($(do_linux_tools),true)
 	$(call dh_all,$(pkgtools))
+	dh_installdirs -p$(metapkgtools)
+	dh_installdocs -p$(metapkgtools)
+	dh_installchangelogs -p$(metapkgtools)
+	dh_compress -p$(metapkgtools)
+	dh_fixperms -p$(metapkgtools)
+	dh_installdeb -p$(metapkgtools)
+	$(lockme) dh_gencontrol -p$(metapkgtools)
+	dh_md5sums -p$(metapkgtools)
+	dh_builddeb -p$(metapkgtools)
 endif
 ifeq ($(do_cloud_tools),true)
 	$(call dh_all,$(pkgcloud))
