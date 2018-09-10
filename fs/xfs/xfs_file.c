89e640c3c0c97b982c57032c8811fedd821bc3b1
Dave Jiang <dave.jiang@intel.com>
2018-09-10 14:47:45 -0400

    xfs: Close race between direct IO and xfs_break_layouts()
    BugLink: https://bugs.launchpad.net/bugs/1787089
    
    This patch is the duplicate of ross's fix for ext4 for xfs.
    
    If the refcount of a page is lowered between the time that it is returned
    by dax_busy_page() and when the refcount is again checked in
    xfs_break_layouts() => ___wait_var_event(), the waiting function
    xfs_wait_dax_page() will never be called.  This means that
    xfs_break_layouts() will still have 'retry' set to false, so we'll stop
    looping and never check the refcount of other pages in this inode.
    
    Instead, always continue looping as long as dax_layout_busy_page() gives us
    a page which it found with an elevated refcount.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    (backported from commit e25ff835af89a80aa6a4de58f413e494b2b96bd1)
    Signed-off-by: Joseph Salisbury <joseph.salisbury@canonical.com>
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>

--- fs/xfs/xfs_file.c
+++ fs/xfs/xfs_file.c
@@ -721,12 +721,10 @@ xfs_file_write_iter(
 
 static void
 xfs_wait_dax_page(
-	struct inode		*inode,
-	bool			*did_unlock)
+	struct inode		*inode)
 {
 	struct xfs_inode        *ip = XFS_I(inode);
 
-	*did_unlock = true;
 	xfs_iunlock(ip, XFS_MMAPLOCK_EXCL);
 	schedule();
 	xfs_ilock(ip, XFS_MMAPLOCK_EXCL);
@@ -736,7 +734,7 @@ static int
 xfs_break_dax_layouts(
 	struct inode		*inode,
 	uint			iolock,
-	bool			*did_unlock)
+	bool			*retry)
 {
 	struct page		*page;
 
@@ -746,9 +744,10 @@ xfs_break_dax_layouts(
 	if (!page)
 		return 0;
 
+	*retry = true;
 	return ___wait_var_event(&page->_refcount,
 			atomic_read(&page->_refcount) == 1, TASK_INTERRUPTIBLE,
-			0, 0, xfs_wait_dax_page(inode, did_unlock));
+			0, 0, xfs_wait_dax_page(inode));
 }
 
 int
