1a76921d1a8162e8c5054c80fb47d68f20e90667
Seth Forshee <seth.forshee@canonical.com>
2018-10-24 14:12:42 -0600

    Revert "UBUNTU: SAUCE: (efi-lockdown) x86: Lock down IO port access when the kernel is locked down"
    This reverts commit dd0f1c9d2555236467d6b37141d128f0cfd8b879.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- arch/x86/kernel/ioport.c
+++ arch/x86/kernel/ioport.c
@@ -31,8 +31,7 @@ long ksys_ioperm(unsigned long from, unsigned long num, int turn_on)
 
 	if ((from + num <= from) || (from + num > IO_BITMAP_BITS))
 		return -EINVAL;
-	if (turn_on && (!capable(CAP_SYS_RAWIO) ||
-			kernel_is_locked_down("ioperm")))
+	if (turn_on && !capable(CAP_SYS_RAWIO))
 		return -EPERM;
 
 	/*
@@ -127,8 +126,7 @@ SYSCALL_DEFINE1(iopl, unsigned int, level)
 		return -EINVAL;
 	/* Trying to gain more privileges? */
 	if (level > old) {
-		if (!capable(CAP_SYS_RAWIO) ||
-		    kernel_is_locked_down("iopl"))
+		if (!capable(CAP_SYS_RAWIO))
 			return -EPERM;
 	}
 	regs->flags = (regs->flags & ~X86_EFLAGS_IOPL) |
