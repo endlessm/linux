4c35b1194da2ac63b7fd1328d404d77b2348a8d3
Seth Forshee <seth.forshee@canonical.com>
2018-10-24 14:12:30 -0600

    Revert "UBUNTU: SAUCE: (efi-lockdown) Add efi_status_to_str() and rework efi_status_to_err()."
    This reverts commit 20ed1e0bf80dff4c285cf89ddf25def3d7fc60d2.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- include/linux/efi.h
+++ include/linux/efi.h
@@ -42,8 +42,6 @@
 #define EFI_ABORTED		(21 | (1UL << (BITS_PER_LONG-1)))
 #define EFI_SECURITY_VIOLATION	(26 | (1UL << (BITS_PER_LONG-1)))
 
-#define EFI_IS_ERROR(x)		((x) & (1UL << (BITS_PER_LONG-1)))
-
 typedef unsigned long efi_status_t;
 typedef u8 efi_bool_t;
 typedef u16 efi_char16_t;		/* UNICODE character */
@@ -1231,7 +1229,6 @@ static inline void efi_set_secure_boot(enum efi_secureboot_mode mode) {}
 #endif
 
 extern int efi_status_to_err(efi_status_t status);
-extern const char *efi_status_to_str(efi_status_t status);
 
 /*
  * Variable Attributes
