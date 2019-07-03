e8e24ce7545c01d92e5a0bd88aed7c7c06bf938c
Daniel Drake <drake@endlessm.com>
2019-07-03 14:26:50 +0800

    rtl8821ce: always indicate disconnect during netdev_close
    In eos3.5, CONFIG_ANDROID=n, so we were running this code, which
    we've previously documented as being the essential thing to tell
    cfg80211 that we've disconnected in the suspend/resume path in the
    rtl8723bu case.
    
    In eos3.6, CONFIG_ANDROID=y so we were no longer doing this.
    After suspend/resume, wifi reconnection is broken as a result.
    
    "Fix" this by restoring the eos3.5 behaviour of running this codepath.
    
    There is a flag that looks like a better fix -
    RTW_CFG80211_ALWAYS_INFORM_STA_DISCONNECT_EVENT - but we are instead
    tweaking the ifdefs here as it is a less invasive/risky change, restoring
    the codepath that we previously shipped.
    
    https://phabricator.endlessm.com/T20835
    https://phabricator.endlessm.com/T27112

--- drivers/net/wireless/rtl8821ce/os_dep/linux/os_intfs.c
+++ drivers/net/wireless/rtl8821ce/os_dep/linux/os_intfs.c
@@ -3192,7 +3192,6 @@ static int netdev_close(struct net_device *pnetdev)
 		if (pnetdev)
 			rtw_netif_stop_queue(pnetdev);
 
-#ifndef CONFIG_ANDROID
 		/* s2. */
 		LeaveAllPowerSaveMode(padapter);
 		rtw_disassoc_cmd(padapter, 500, RTW_CMDF_DIRECTLY);
@@ -3202,7 +3201,6 @@ static int netdev_close(struct net_device *pnetdev)
 		rtw_free_assoc_resources(padapter, 1);
 		/* s2-4. */
 		rtw_free_network_queue(padapter, _TRUE);
-#endif
 		/* Close LED */
 		rtw_led_control(padapter, LED_CTL_POWER_OFF);
 	}
