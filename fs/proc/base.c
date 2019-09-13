b76f8ccadba6c1f9641fa513404ae7e203981949
Derek Foreman <derek@endlessm.com>
2019-09-13 16:40:09 -0500

    [Endless] endlesspayg: Block proc populating for LSM user
    Stop the pid directory in proc from being populated for the holder
    of the LSM
    
    https://phabricator.endlessm.com/T27044

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -102,6 +102,7 @@
 #include "fd.h"
 
 #include "../../lib/kstrtox.h"
+#include "../../security/endlesspayg/endlesspayg.h"
 
 /* NOTE:
  *	Implementing inode permission operations in /proc is almost
@@ -2636,6 +2637,8 @@ static struct dentry *proc_pident_lookup(struct inode *dir,
 	if (!task)
 		goto out_no_task;
 
+	if (!eospayg_proc_pid_is_safe(task->pid))
+		goto out_payg_filtered;
 	/*
 	 * Yes, it does not scale. And it should not. Don't add
 	 * new entries into /proc/<tgid>/ without very good reasons.
@@ -2648,6 +2651,7 @@ static struct dentry *proc_pident_lookup(struct inode *dir,
 			break;
 		}
 	}
+out_payg_filtered:
 	put_task_struct(task);
 out_no_task:
 	return res;
@@ -2661,6 +2665,9 @@ static int proc_pident_readdir(struct file *file, struct dir_context *ctx,
 
 	if (!task)
 		return -ENOENT;
+
+	if (!eospayg_proc_pid_is_safe(task->pid))
+		goto out;
 
 	if (!dir_emit_dots(file, ctx))
 		goto out;
