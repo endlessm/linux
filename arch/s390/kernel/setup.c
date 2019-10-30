4528363a45cea244a77f41c88662f39f6c1bbc4b
Jeremy Cline <jcline@redhat.com>
2019-10-30 14:37:49 +0000

    UBUNTU: SAUCE: (lockdown) s390: Lock down the kernel when the IPL secure flag is set
    Automatically lock down the kernel to LOCKDOWN_CONFIDENTIALITY_MAX if
    the IPL secure flag is set.
    
    Upstream Status: RHEL only
    Suggested-by: Philipp Rudo <prudo@redhat.com>
    Signed-off-by: Jeremy Cline <jcline@redhat.com>
    (cherry picked from commit 2384646bf71d8c282cf49bb20321fdf802c61cce
    https://gitlab.com/cki-project/kernel-ark)
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- arch/s390/kernel/setup.c
+++ arch/s390/kernel/setup.c
@@ -51,6 +51,7 @@
 #include <linux/start_kernel.h>
 #include <linux/hugetlb.h>
 #include <linux/kmemleak.h>
+#include <linux/security.h>
 
 #include <asm/boot_data.h>
 #include <asm/ipl.h>
@@ -969,6 +970,9 @@ void __init setup_arch(char **cmdline_p)
 		pr_info("Linux is running as a guest in 64-bit mode\n");
 
 	log_component_list();
+
+	if (ipl_get_secureboot())
+		security_lock_kernel_down("Secure IPL mode", LOCKDOWN_INTEGRITY_MAX);
 
 	/* Have one command line that is parsed and saved in /proc/cmdline */
 	/* boot_command_line has been already set up in early.c */
