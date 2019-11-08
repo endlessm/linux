7fe62dcdfeb8d2266049001d6d1962311b900a25
João Paulo Rechi Vita <jprvita@gmail.com>
2019-11-07 23:37:11 -0800

    fixup! ACPI: EC: add support for hardware-reduced systems
    This change was somehow dropped when I rebased the commit in question
    after merging a minor update from Ubuntu.

--- drivers/acpi/internal.h
+++ drivers/acpi/internal.h
@@ -166,6 +166,7 @@ static inline void acpi_early_processor_osc(void) {}
 struct acpi_ec {
 	acpi_handle handle;
 	u32 gpe;
+	int irq;
 	unsigned long command_addr;
 	unsigned long data_addr;
 	bool global_lock;
