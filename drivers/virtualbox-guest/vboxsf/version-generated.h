dfa5a7fc213441da2b16414c334ae54c6d65a906
João Paulo Rechi Vita <jprvita@endlessm.com>
2019-12-03 15:12:18 -0800

    Import virtualbox-guest-source version 6.0.14-dfsg-3
    Import virtualbox-guest drivers to provide a good user experience when
    running inside a VirtualBox VM as a guest OS.
    
    This is an unchanged import of the tarball installed by the package
    'virtualbox-guest-source', version 6.0.14-dfsg-3 from Debian, achieved
    by running the following command after installing the package:
    
     tar -C drivers --strip-components=1 -xvf /usr/src/virtualbox-guest.tar.bz2
    
    Build integration will follow on a separate commit.
    
    https://phabricator.endlessm.com/T28495
    
    Signed-off-by: João Paulo Rechi Vita <jprvita@endlessm.com>

---
+++ drivers/virtualbox-guest/vboxsf/version-generated.h
@@ -0,0 +1,13 @@
+#ifndef ___version_generated_h___
+#define ___version_generated_h___
+
+#define VBOX_VERSION_MAJOR 6
+#define VBOX_VERSION_MINOR 0
+#define VBOX_VERSION_BUILD 14
+#define VBOX_VERSION_STRING_RAW "6.0.14"
+#define VBOX_VERSION_STRING "6.0.14_Debian"
+#define VBOX_API_VERSION_STRING "6_0"
+
+#define VBOX_PRIVATE_BUILD_DESC "Private build by buildd"
+
+#endif
