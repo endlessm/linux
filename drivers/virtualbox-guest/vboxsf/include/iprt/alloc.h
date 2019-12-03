0777d338de35ccc57e90351e774e18dd40037566
João Paulo Rechi Vita <jprvita@endlessm.com>
2019-12-03 15:12:18 -0800

    Import virtualbox-guest-source version 6.0.14-dfsg-3
    Import virtualbox-guest drivers to provide a good user experience when
    running inside a VirtualBox VM as a guest OS.
    
    This is an unchanged import of the tarball installed by the package
    'virtualbox-guest-source', version 6.0.14-dfsg-3 from Debian, achieved
    by running the following command after installing the package:
    
     tar -C drivers --strip-components=1 -xvf /usr/src/virtualbox-guest.tar.bz2
    
    Build integration will follow on a separate commit.
    
    https://phabricator.endlessm.com/T28495
    
    Signed-off-by: João Paulo Rechi Vita <jprvita@endlessm.com>

---
+++ drivers/virtualbox-guest/vboxsf/include/iprt/alloc.h
@@ -0,0 +1,36 @@
+/** @file
+ * IPRT - Memory Allocation.
+ */
+
+/*
+ * Copyright (C) 2006-2019 Oracle Corporation
+ *
+ * This file is part of VirtualBox Open Source Edition (OSE), as
+ * available from http://www.virtualbox.org. This file is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) as published by the Free Software
+ * Foundation, in version 2 as it comes in the "COPYING" file of the
+ * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
+ *
+ * The contents of this file may alternatively be used under the terms
+ * of the Common Development and Distribution License Version 1.0
+ * (CDDL) only, as it comes in the "COPYING.CDDL" file of the
+ * VirtualBox OSE distribution, in which case the provisions of the
+ * CDDL are applicable instead of those of the GPL.
+ *
+ * You may elect to license modified versions of this file under the
+ * terms and conditions of either the GPL or the CDDL or both.
+ */
+
+#ifndef IPRT_INCLUDED_alloc_h
+#define IPRT_INCLUDED_alloc_h
+#ifndef RT_WITHOUT_PRAGMA_ONCE
+# pragma once
+#endif
+
+/* Forwarding to the canonical header. */
+#include <iprt/mem.h>
+
+#endif /* !IPRT_INCLUDED_alloc_h */
+
