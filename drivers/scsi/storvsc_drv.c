2d90015ce6d2cfe9877838970714cdbc6203ad6b
Long Li <longli@microsoft.com>
2020-01-13 16:08:36 -0800

    scsi: storvsc: Correctly set number of hardware queues for IDE disk
    BugLink: https://bugs.launchpad.net/bugs/1861090
    
    commit 7b571c19d4c0b78d27dd3bf1f3c42e4032390af6 upstream.
    
    Commit 0ed881027690 ("scsi: storvsc: setup 1:1 mapping between hardware
    queue and CPU queue") introduced a regression for disks attached to
    IDE. For these disks the host VSP only offers one VMBUS channel. Setting
    multiple queues can overload the VMBUS channel and result in performance
    drop for high queue depth workload on system with large number of CPUs.
    
    Fix it by leaving the number of hardware queues to 1 (default value) for
    IDE disks.
    
    Fixes: 0ed881027690 ("scsi: storvsc: setup 1:1 mapping between hardware queue and CPU queue")
    Link: https://lore.kernel.org/r/1578960516-108228-1-git-send-email-longli@linuxonhyperv.com
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Long Li <longli@microsoft.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- drivers/scsi/storvsc_drv.c
+++ drivers/scsi/storvsc_drv.c
@@ -1841,9 +1841,11 @@ static int storvsc_probe(struct hv_device *device,
 #endif
 
 	/*
+	 * For non-IDE disks, the host supports multiple channels.
 	 * Set the number of HW queues we are supporting.
 	 */
-	host->nr_hw_queues = num_present_cpus();
+	if (!dev_is_ide)
+		host->nr_hw_queues = num_present_cpus();
 
 	/*
 	 * Set the error handler work queue.
