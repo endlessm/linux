d946de8ee51bcf645b9d28a21a987f01cb44531b
Javier Martinez Canillas <javierm@redhat.com>
2020-02-17 12:39:47 +0100

    efi: Only print errors about failing to get certs if EFI vars are found
    BugLink: https://bugs.launchpad.net/bugs/1867178
    
    [ Upstream commit 3be54d558c75562e42bc83d665df024bd79d399b ]
    
    If CONFIG_LOAD_UEFI_KEYS is enabled, the kernel attempts to load the certs
    from the db, dbx and MokListRT EFI variables into the appropriate keyrings.
    
    But it just assumes that the variables will be present and prints an error
    if the certs can't be loaded, even when is possible that the variables may
    not exist. For example the MokListRT variable will only be present if shim
    is used.
    
    So only print an error message about failing to get the certs list from an
    EFI variable if this is found. Otherwise these printed errors just pollute
    the kernel log ring buffer with confusing messages like the following:
    
    [    5.427251] Couldn't get size: 0x800000000000000e
    [    5.427261] MODSIGN: Couldn't get UEFI db list
    [    5.428012] Couldn't get size: 0x800000000000000e
    [    5.428023] Couldn't get UEFI MokListRT
    
    Reported-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>
    Tested-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- security/integrity/platform_certs/load_uefi.c
+++ security/integrity/platform_certs/load_uefi.c
@@ -39,36 +39,36 @@ static __init bool uefi_check_ignore_db(void)
  * Get a certificate list blob from the named EFI variable.
  */
 static __init int get_cert_list(efi_char16_t *name, efi_guid_t *guid,
-				  unsigned long *size , void **cert_list)
+				  unsigned long *size , void **cert_list,
+				  efi_status_t *status)
 {
-	efi_status_t status;
 	unsigned long lsize = 4;
 	unsigned long tmpdb[4];
 	void *db;
 
-	status = efi.get_variable(name, guid, NULL, &lsize, &tmpdb);
-	if (status == EFI_NOT_FOUND) {
+	*status = efi.get_variable(name, guid, NULL, &lsize, &tmpdb);
+	if (*status == EFI_NOT_FOUND) {
 		*size = 0;
 		*cert_list = NULL;
 		return 0;
 	}
 
-	if (status != EFI_BUFFER_TOO_SMALL) {
+	if (*status != EFI_BUFFER_TOO_SMALL) {
 		pr_err("Couldn't get size: %s (0x%lx)\n",
-		       efi_status_to_str(status), status);
-		return efi_status_to_err(status);
+		       efi_status_to_str(*status), *status);
+		return efi_status_to_err(*status);
 	}
 
 	db = kmalloc(lsize, GFP_KERNEL);
 	if (!db)
 		return -ENOMEM;
 
-	status = efi.get_variable(name, guid, NULL, &lsize, db);
-	if (status != EFI_SUCCESS) {
+	*status = efi.get_variable(name, guid, NULL, &lsize, db);
+	if (*status != EFI_SUCCESS) {
 		kfree(db);
 		pr_err("Error reading db var: %s (0x%lx)\n",
-		       efi_status_to_str(status), status);
-		return efi_status_to_err(status);
+		       efi_status_to_str(*status), *status);
+		return efi_status_to_err(*status);
 	}
 
 	*size = lsize;
@@ -153,6 +153,7 @@ static int __init load_uefi_certs(void)
 	efi_guid_t mok_var = EFI_SHIM_LOCK_GUID;
 	void *db = NULL, *dbx = NULL, *mok = NULL;
 	unsigned long dbsize = 0, dbxsize = 0, moksize = 0;
+	efi_status_t status;
 	int rc = 0;
 
 	if (!efi.get_variable)
@@ -162,9 +163,12 @@ static int __init load_uefi_certs(void)
 	 * an error if we can't get them.
 	 */
 	if (!uefi_check_ignore_db()) {
-		rc = get_cert_list(L"db", &secure_var, &dbsize, &db);
+		rc = get_cert_list(L"db", &secure_var, &dbsize, &db, &status);
 		if (rc < 0) {
-			pr_err("MODSIGN: Couldn't get UEFI db list\n");
+			if (status == EFI_NOT_FOUND)
+				pr_debug("MODSIGN: db variable wasn't found\n");
+			else
+				pr_err("MODSIGN: Couldn't get UEFI db list\n");
 		} else if (dbsize != 0) {
 			rc = parse_efi_signature_list("UEFI:db",
 					db, dbsize, get_handler_for_db);
@@ -175,9 +179,12 @@ static int __init load_uefi_certs(void)
 		}
 	}
 
-	rc = get_cert_list(L"MokListRT", &mok_var, &moksize, &mok);
+	rc = get_cert_list(L"MokListRT", &mok_var, &moksize, &mok, &status);
 	if (rc < 0) {
-		pr_info("Couldn't get UEFI MokListRT\n");
+		if (status == EFI_NOT_FOUND)
+			pr_debug("MokListRT variable wasn't found\n");
+		else
+			pr_info("Couldn't get UEFI MokListRT\n");
 	} else if (moksize != 0) {
 		rc = parse_efi_signature_list("UEFI:MokListRT",
 					      mok, moksize, get_handler_for_db);
@@ -186,9 +193,12 @@ static int __init load_uefi_certs(void)
 		kfree(mok);
 	}
 
-	rc = get_cert_list(L"dbx", &secure_var, &dbxsize, &dbx);
+	rc = get_cert_list(L"dbx", &secure_var, &dbxsize, &dbx, &status);
 	if (rc < 0) {
-		pr_info("Couldn't get UEFI dbx list\n");
+		if (status == EFI_NOT_FOUND)
+			pr_debug("dbx variable wasn't found\n");
+		else
+			pr_info("Couldn't get UEFI dbx list\n");
 	} else if (dbxsize != 0) {
 		rc = parse_efi_signature_list("UEFI:dbx",
 					      dbx, dbxsize,
