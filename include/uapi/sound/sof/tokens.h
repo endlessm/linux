eb26b7f2e41be5ba46beb584c32cc73328d75b6d
Jaska Uimonen <jaska.uimonen@linux.intel.com>
2020-02-24 09:13:10 +0800

    ASoC: SOF: acpi led support for switch controls
    BugLink: https://bugs.launchpad.net/bugs/1864398
    
    Currently sof doesn't support acpi leds with mute switches. So implement
    acpi leds following quite shamelessly existing HDA implementation by
    Takashi Iwai.
    
    Mute leds can be enabled in topology by adding led and direction token
    in switch control private data.
    
    Signed-off-by: Jaska Uimonen <jaska.uimonen@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20191008164443.1358-10-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 5d43001ae43606dc525f55c482c545afba01bb55)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- include/uapi/sound/sof/tokens.h
+++ include/uapi/sound/sof/tokens.h
@@ -114,4 +114,8 @@
 #define SOF_TKN_IMX_ESAI_FIRST_TOKEN		1100
 /* TODO: Add ESAI tokens */
 
+/* Led control for mute switches */
+#define SOF_TKN_MUTE_LED_USE			1300
+#define SOF_TKN_MUTE_LED_DIRECTION		1301
+
 #endif
