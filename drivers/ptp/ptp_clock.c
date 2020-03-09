7c78a0d3b68b7ef48e44fd14b410a0a479a31141
Andrea Righi <andrea.righi@canonical.com>
2020-03-09 18:22:40 +0100

    UBUNTU: SAUCE: ptp: free ptp clock properly
    There is a bug in ptp_clock_unregister() where pps_unregister_source()
    can free up resources needed by posix_clock_unregister() to properly
    destroy a related sysfs device.
    
    Fix this by calling pps_unregister_source() in ptp_clock_release().
    
    See also:
    commit 75718584cb3c ("ptp: free ptp device pin descriptors properly").
    
    BugLink: https://bugs.launchpad.net/bugs/1864754
    Fixes: a33121e5487b ("ptp: fix the race between the release of ptp_clock and cdev")
    Tested-by: Piotr Morgwai Kotarbiński <foss@morgwai.pl>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- drivers/ptp/ptp_clock.c
+++ drivers/ptp/ptp_clock.c
@@ -179,6 +179,9 @@ static void ptp_clock_release(struct device *dev)
 {
 	struct ptp_clock *ptp = container_of(dev, struct ptp_clock, dev);
 
+	/* Release the clock's resources. */
+	if (ptp->pps_source)
+		pps_unregister_source(ptp->pps_source);
 	ptp_cleanup_pin_groups(ptp);
 	mutex_destroy(&ptp->tsevq_mux);
 	mutex_destroy(&ptp->pincfg_mux);
@@ -307,11 +310,6 @@ int ptp_clock_unregister(struct ptp_clock *ptp)
 		kthread_cancel_delayed_work_sync(&ptp->aux_work);
 		kthread_destroy_worker(ptp->kworker);
 	}
-
-	/* Release the clock's resources. */
-	if (ptp->pps_source)
-		pps_unregister_source(ptp->pps_source);
-
 	posix_clock_unregister(&ptp->clock);
 
 	return 0;
