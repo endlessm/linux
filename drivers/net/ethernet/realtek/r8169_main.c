8837218109148eb30052b2179a2ba31950a554a6
AceLan Kao <acelan.kao@canonical.com>
2020-03-10 09:48:36 +0800

    UBUNTU: SAUCE: r8169: disable ASPM L1.1
    BguLink: https://bugs.launchpad.net/bugs/1836030
    
    r8169 doesn't suport ASPM L1.1, so we don't have to disable ASPM
    completely. Disable ASPM L1.1 doesn't affect the power consumption and
    the network function keeps working after S3 test 30 times.
    
    Signed-off-by: AceLan Kao <acelan.kao@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- drivers/net/ethernet/realtek/r8169_main.c
+++ drivers/net/ethernet/realtek/r8169_main.c
@@ -5281,8 +5281,7 @@ static int rtl_init_one(struct pci_dev *pdev, const struct pci_device_id *ent)
 	/* Disable ASPM completely as that cause random device stop working
 	 * problems as well as full system hangs for some PCIe devices users.
 	 */
-	rc = pci_disable_link_state(pdev, PCIE_LINK_STATE_L0S |
-					  PCIE_LINK_STATE_L1);
+	rc = pci_disable_link_state(pdev, PCIE_LINK_STATE_L1_1);
 	tp->aspm_manageable = !rc;
 
 	/* enable device (incl. PCI PM wakeup and hotplug setup) */
