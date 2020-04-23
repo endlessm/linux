6f18a8434050333afc80b5bfce2e0e994c86b790
Andrei Vagin <avagin@gmail.com>
2020-04-23 12:11:00 +0200

    UBUNTU: SAUCE: overlayfs: use shiftfs hacks only with shiftfs as underlay
    BugLink: https://bugs.launchpad.net/bugs/1857257
    
    The hack was introduced in ("UBUNTU: SAUCE: overlayfs: allow with
    shiftfs as underlay") and it broke checkpoint/restore of docker
    contains:
    https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1857257
    
    The following script can be used to trigger the issue:
      #!/bin/bash
    
      cat > test.py << EOF
      import sys
    
      f = open("/proc/self/maps")
    
      for l in f.readlines():
        if "python" not in l:
          continue
        print(l)
        s = l.split()
        start, end = s[0].split("-")
        fname = s[-1]
        print(start, end, fname)
        break
      else:
        sys.exit(1)
    
      test_file1 = open(fname)
      test_file2 = open("/proc/self/map_files/%s-%s" % (start, end))
    
      fdinfo1 = open("/proc/self/fdinfo/%d" % test_file1.fileno()).read()
      fdinfo2 = open("/proc/self/fdinfo/%d" % test_file2.fileno()).read()
    
      if fdinfo1 != fdinfo2:
        print("FAIL")
        print(test_file1)
        print(fdinfo1)
        print(test_file2)
        print(fdinfo2)
        sys.exit(1)
      print("PASS")
      EOF
      sudo docker run -it --privileged --rm -v `pwd`:/mnt python python /mnt/test.py
    
    Cc: Stefan Bader <stefan.bader@canonical.com>
    Cc: Connor Kuehl <connor.kuehl@canonical.com>
    Cc: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Fixes: 58009298c6bd ("UBUNTU: SAUCE: overlayfs: allow with shiftfs as underlay")
    Acked-by: Christian Brauner <christian.brauner@ubuntu.com>
    Signed-off-by: Andrei Vagin <avagin@gmail.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Acked-by: Kamal Mostafa <kamal@canonical.com>
    [ klebers: fixed compilation error by including <uapi/linux/magic.h>.]
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- fs/overlayfs/file.c
+++ fs/overlayfs/file.c
@@ -3,6 +3,7 @@
  * Copyright (C) 2017 Red Hat, Inc.
  */
 
+#include <uapi/linux/magic.h>
 #include <linux/cred.h>
 #include <linux/file.h>
 #include <linux/mount.h>
@@ -31,9 +32,12 @@ static struct file *ovl_open_realfile(const struct file *file,
 	int flags = file->f_flags | O_NOATIME | FMODE_NONOTIFY;
 
 	old_cred = ovl_override_creds(inode->i_sb);
-	ovl_path_real(file->f_path.dentry, &realpath);
-	realfile = open_with_fake_path(&realpath, flags, realinode,
-				       current_cred());
+	if (realpath.dentry->d_sb->s_magic == SHIFTFS_MAGIC)
+		realfile = open_with_fake_path(&realpath, flags, realinode,
+					       current_cred());
+	else
+		realfile = open_with_fake_path(&file->f_path, flags, realinode,
+					       current_cred());
 	revert_creds(old_cred);
 
 	pr_debug("open(%p[%pD2/%c], 0%o) -> (%p, 0%o)\n",
