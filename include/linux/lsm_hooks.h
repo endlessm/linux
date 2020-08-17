5748a051557105300c4d0c14bc70ca3ec604fe77
Casey Schaufler <casey@schaufler-ca.com>
2020-08-17 16:02:56 -0700

    UBUNTU: SAUCE: LSM: Create and manage the lsmblob data structure.
    When more than one security module is exporting data to
    audit and networking sub-systems a single 32 bit integer
    is no longer sufficient to represent the data. Add a
    structure to be used instead.
    
    The lsmblob structure is currently an array of
    u32 "secids". There is an entry for each of the
    security modules built into the system that would
    use secids if active. The system assigns the module
    a "slot" when it registers hooks. If modules are
    compiled in but not registered there will be unused
    slots.
    
    A new lsm_id structure, which contains the name
    of the LSM and its slot number, is created. There
    is an instance for each LSM, which assigns the name
    and passes it to the infrastructure to set the slot.
    
    The audit rules data is expanded to use an array of
    security module data rather than a single instance.
    Because IMA uses the audit rule functions it is
    affected as well.
    
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Acked-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    [ saf: resolve conflicts ]
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    [ update to support landlock ]
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -1603,6 +1603,14 @@ struct security_hook_heads {
 } __randomize_layout;
 
 /*
+ * Information that identifies a security module.
+ */
+struct lsm_id {
+	const char	*lsm;	/* Name of the LSM */
+	int		slot;	/* Slot in lsmblob if one is allocated */
+};
+
+/*
  * Security module hook list structure.
  * For use with generic list macros for common operations.
  */
@@ -1610,7 +1618,7 @@ struct security_hook_list {
 	struct hlist_node		list;
 	struct hlist_head		*head;
 	union security_list_options	hook;
-	const char			*lsm;
+	struct lsm_id			*lsmid;
 } __randomize_layout;
 
 /*
@@ -1646,7 +1654,7 @@ extern struct security_hook_heads security_hook_heads;
 extern char *lsm_names;
 
 extern void security_add_hooks(struct security_hook_list *hooks, int count,
-				const char *lsm);
+			       struct lsm_id *lsmid);
 
 #define LSM_FLAG_LEGACY_MAJOR	BIT(0)
 #define LSM_FLAG_EXCLUSIVE	BIT(1)
