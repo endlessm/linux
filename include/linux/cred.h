ac977abb748fb510723138e53100a338c916f45a
Casey Schaufler <casey@schaufler-ca.com>
2020-08-18 10:12:56 -0700

    UBUNTU: SAUCE: LSM: Use lsmblob in security_kernel_act_as
    Change the security_kernel_act_as interface to use a lsmblob
    structure in place of the single u32 secid in support of
    module stacking. Change its only caller, set_security_override,
    to do the same. Change that one's only caller,
    set_security_override_from_ctx, to call it with the new
    parameter type.
    
    The security module hook is unchanged, still taking a secid.
    The infrastructure passes the correct entry from the lsmblob.
    lsmblob_init() is used to fill the lsmblob structure, however
    this will be removed later in the series when security_secctx_to_secid()
    is undated to provide a lsmblob instead of a secid.
    
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- include/linux/cred.h
+++ include/linux/cred.h
@@ -18,6 +18,7 @@
 
 struct cred;
 struct inode;
+struct lsmblob;
 
 /*
  * COW Supplementary groups list
@@ -165,7 +166,7 @@ extern const struct cred *override_creds(const struct cred *);
 extern void revert_creds(const struct cred *);
 extern struct cred *prepare_kernel_cred(struct task_struct *);
 extern int change_create_files_as(struct cred *, struct inode *);
-extern int set_security_override(struct cred *, u32);
+extern int set_security_override(struct cred *, struct lsmblob *);
 extern int set_security_override_from_ctx(struct cred *, const char *);
 extern int set_create_files_as(struct cred *, struct inode *);
 extern int cred_fscmp(const struct cred *, const struct cred *);
