ffa754bb807c669abfeb852b9ed178275467f3f5
Casey Schaufler <casey@schaufler-ca.com>
2020-08-19 16:06:37 -0700

    UBUNTU: SAUCE: LSM: Use lsmblob in security_task_getsecid
    Change the security_task_getsecid() interface to fill in
    a lsmblob structure instead of a u32 secid in support of
    LSM stacking. Audit interfaces will need to collect all
    possible secids for possible reporting.
    
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    cc: linux-integrity@vger.kernel.org
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    [ saf: resolve conflicts ]
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -2697,20 +2697,10 @@ static void binder_transaction(struct binder_proc *proc,
 	t->priority = task_nice(current);
 
 	if (target_node && target_node->txn_security_ctx) {
-		u32 secid;
 		struct lsmblob blob;
 		size_t added_size;
 
-		security_task_getsecid(proc->tsk, &secid);
-		/*
-		 * Later in this patch set security_task_getsecid() will
-		 * provide a lsmblob instead of a secid. lsmblob_init
-		 * is used to ensure that all the secids in the lsmblob
-		 * get the value returned from security_task_getsecid(),
-		 * which means that the one expected by
-		 * security_secid_to_secctx() will be set.
-		 */
-		lsmblob_init(&blob, secid);
+		security_task_getsecid(proc->tsk, &blob);
 		ret = security_secid_to_secctx(&blob, &secctx, &secctx_sz);
 		if (ret) {
 			return_error = BR_FAILED_REPLY;
