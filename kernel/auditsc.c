c15816f6f45f9742bccc5d6ab489029d9f8a65ce
Casey Schaufler <casey@schaufler-ca.com>
2020-08-20 11:47:01 -0700

    UBUNTU: SAUCE: LSM: Ensure the correct LSM context releaser
    Add a new lsmcontext data structure to hold all the information
    about a "security context", including the string, its size and
    which LSM allocated the string. The allocation information is
    necessary because LSMs have different policies regarding the
    lifecycle of these strings. SELinux allocates and destroys
    them on each use, whereas Smack provides a pointer to an entry
    in a list that never goes away.
    
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Cc: linux-integrity@vger.kernel.org
    Cc: netdev@vger.kernel.org
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -1099,6 +1099,7 @@ static int audit_log_pid_context(struct audit_context *context, pid_t pid,
 				 struct lsmblob *blob, char *comm)
 {
 	struct audit_buffer *ab;
+	struct lsmcontext lsmcxt;
 	char *ctx = NULL;
 	u32 len;
 	int rc = 0;
@@ -1116,7 +1117,8 @@ static int audit_log_pid_context(struct audit_context *context, pid_t pid,
 			rc = 1;
 		} else {
 			audit_log_format(ab, " obj=%s", ctx);
-			security_release_secctx(ctx, len);
+			lsmcontext_init(&lsmcxt, ctx, len, 0); /*scaffolding*/
+			security_release_secctx(&lsmcxt);
 		}
 	}
 	audit_log_format(ab, " ocomm=");
@@ -1376,6 +1378,7 @@ static void audit_log_time(struct audit_context *context, struct audit_buffer **
 
 static void show_special(struct audit_context *context, int *call_panic)
 {
+	struct lsmcontext lsmcxt;
 	struct audit_buffer *ab;
 	int i;
 
@@ -1410,7 +1413,8 @@ static void show_special(struct audit_context *context, int *call_panic)
 				*call_panic = 1;
 			} else {
 				audit_log_format(ab, " obj=%s", ctx);
-				security_release_secctx(ctx, len);
+				lsmcontext_init(&lsmcxt, ctx, len, 0);
+				security_release_secctx(&lsmcxt);
 			}
 		}
 		if (context->ipc.has_perm) {
@@ -1572,6 +1576,7 @@ static void audit_log_name(struct audit_context *context, struct audit_names *n,
 		char *ctx = NULL;
 		u32 len;
 		struct lsmblob blob;
+		struct lsmcontext lsmcxt;
 
 		lsmblob_init(&blob, n->osid);
 		if (security_secid_to_secctx(&blob, &ctx, &len)) {
@@ -1580,7 +1585,8 @@ static void audit_log_name(struct audit_context *context, struct audit_names *n,
 				*call_panic = 2;
 		} else {
 			audit_log_format(ab, " obj=%s", ctx);
-			security_release_secctx(ctx, len);
+			lsmcontext_init(&lsmcxt, ctx, len, 0); /* scaffolding */
+			security_release_secctx(&lsmcxt);
 		}
 	}
 
