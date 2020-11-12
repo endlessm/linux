d62a49b066291b63b7c0dfcabd81a93b64a4a093
Seth Forshee <seth.forshee@canonical.com>
2020-11-12 12:34:01 -0600

    UBUNTU: [Debian] Build linux-libc-dev for debian.master* branches
    BugLink: https://bugs.launchpad.net/bugs/1904067
    
    We don't build linux-libc-dev if $DEBIAN is not debian.master.
    However, for a master kernel forward ported to the devel series
    we do want to build linux-libc-dev. $DEBIAN will be named
    debian.master-SERIES for these kernels, so allow building
    linux-libc-dev for these kernels too.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -557,7 +557,7 @@ binary-arch-headers: install-arch-headers
 	dh_testdir
 	dh_testroot
 ifeq ($(do_libc_dev_package),true)
-ifneq ($(DEBIAN),debian.master)
+ifeq ($(filter debian.master%,$(DEBIAN)),)
 	echo "non-master branch building linux-libc-dev, aborting"
 	exit 1
 endif
