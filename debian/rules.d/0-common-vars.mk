b8ee69ae1d99ac563c046d1c8d2f4a51bcb21280
Kamal Mostafa <kamal@canonical.com>
2020-12-16 13:40:15 -0800

    UBUNTU: disable building bpf selftests (no VMLINUX_BTF)
    BugLink: https://bugs.launchpad.net/bugs/1908144
    
    Disable selftests/bpf since it cannot be built without having built vmlinux
    first, else build fails with either:
    
        Makefile:...: *** Cannot find a vmlinux for VMLINUX_BTF at any of
        "{paths}".  Stop.
    
    or this more cryptic variant:
    
        Error: failed to load BTF from format: No such file or directory
    
    Reference: "UBUNTU: SAUCE: selftests/bpf: clarify build error if no vmlinux"
    Reference: https://lore.kernel.org/bpf/20201210185233.28091-1-broonie@kernel.org/
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- debian/rules.d/0-common-vars.mk
+++ debian/rules.d/0-common-vars.mk
@@ -275,4 +275,4 @@ custom_override = \
  $(shell if [ -n "$($(1)_$(2))" ]; then echo "$($(1)_$(2))"; else echo "$($(1))"; fi)
 
 # selftests that Ubuntu cares about
-ubuntu_selftests = breakpoints bpf cpu-hotplug efivarfs memfd memory-hotplug mount net ptrace seccomp timers powerpc user ftrace
+ubuntu_selftests = breakpoints cpu-hotplug efivarfs memfd memory-hotplug mount net ptrace seccomp timers powerpc user ftrace
