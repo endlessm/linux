f27bfc9be0f144649d93ff824509a2a4b4f3c188
Kai-Heng Feng <kai.heng.feng@canonical.com>
2021-01-12 18:54:46 +0800

    UBUNTU: SAUCE: drm/dp: Another HP DreamColor panel brigntness fix
    BugLink: https://bugs.launchpad.net/bugs/1911001
    
    Another HP DreamColor panel, which is used by new HP ZBook Fury, needs
    to use DPCD to control brightness.
    
    Once the proper fix [1] is merged, this can be dropped.
    
    [1] https://patchwork.freedesktop.org/series/81702/
    
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- drivers/gpu/drm/drm_dp_helper.c
+++ drivers/gpu/drm/drm_dp_helper.c
@@ -1958,6 +1958,7 @@ static const struct edid_quirk edid_quirk_list[] = {
 	 * controls anyway...). Until we have a better way of detecting these,
 	 * force DPCD backlight mode on all of them.
 	 */
+	{ MFG(0x06, 0xaf), PROD_ID(0x8c, 0xcd), BIT(DP_QUIRK_FORCE_DPCD_BACKLIGHT) },
 	{ MFG(0x06, 0xaf), PROD_ID(0x9b, 0x32), BIT(DP_QUIRK_FORCE_DPCD_BACKLIGHT) },
 	{ MFG(0x06, 0xaf), PROD_ID(0xeb, 0x41), BIT(DP_QUIRK_FORCE_DPCD_BACKLIGHT) },
 	{ MFG(0x30, 0xe4), PROD_ID(0x61, 0x06), BIT(DP_QUIRK_FORCE_DPCD_BACKLIGHT) },
