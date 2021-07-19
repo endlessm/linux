d29f586b9bfb6c2284ffa16f9e2497e62654a8a7
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2021-07-19 18:17:46 -0500

    ALSA: hda: intel-dsp-cfg: add missing ElkhartLake PCI ID
    BugLink: https://bugs.launchpad.net/bugs/1940468
    
    [ Upstream commit 114613f62f42e7cbc1242c4e82076a0153043761 ]
    
    We missed the fact that ElkhartLake platforms have two different PCI
    IDs. We only added one so the SOF driver is never selected by the
    autodetection logic for the missing configuration.
    
    BugLink: https://github.com/thesofproject/linux/issues/2990
    Fixes: cc8f81c7e625 ('ALSA: hda: fix intel DSP config')
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20210719231746.557325-1-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- sound/hda/intel-dsp-config.c
+++ sound/hda/intel-dsp-config.c
@@ -323,6 +323,10 @@ static const struct config_entry config_table[] = {
 		.flags = FLAG_SOF | FLAG_SOF_ONLY_IF_DMIC_OR_SOUNDWIRE,
 		.device = 0x51cc,
 	},
+	{
+		.flags = FLAG_SOF | FLAG_SOF_ONLY_IF_DMIC,
+		.device = 0x4b58,
+	},
 #endif
 
 };
