c28418091521df3c9063c8a8ce0f4f053b25384b
Christoph Hellwig <hch@lst.de>
2021-10-19 08:25:30 +0200

    block: simplify the block device syncing code
    BugLink: https://bugs.launchpad.net/bugs/1972905
    
    [ Upstream commit 1e03a36bdff4709c1bbf0f57f60ae3f776d51adf ]
    
    Get rid of the indirections and just provide a sync_bdevs
    helper for the generic sync code.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20211019062530.2174626-8-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- fs/internal.h
+++ fs/internal.h
@@ -24,7 +24,6 @@ struct pipe_inode_info;
 extern void __init bdev_cache_init(void);
 
 extern int __sync_blockdev(struct block_device *bdev, int wait);
-void iterate_bdevs(void (*)(struct block_device *, void *), void *);
 void emergency_thaw_bdev(struct super_block *sb);
 #else
 static inline void bdev_cache_init(void)
@@ -34,10 +33,6 @@ static inline void bdev_cache_init(void)
 static inline int __sync_blockdev(struct block_device *bdev, int wait)
 {
 	return 0;
-}
-static inline void iterate_bdevs(void (*f)(struct block_device *, void *),
-		void *arg)
-{
 }
 static inline int emergency_thaw_bdev(struct super_block *sb)
 {
