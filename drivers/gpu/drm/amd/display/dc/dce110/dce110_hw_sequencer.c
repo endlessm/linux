8ceb47e8bbf46fe92f8842ff771a0323d928a98e
Leo (Hanghong) Ma <hanghong.ma@amd.com>
2021-11-22 20:34:10 +0200

    drm/amd/display: Add helper for blanking all dp displays
    BugLink: https://bugs.launchpad.net/bugs/1951868
    
    [Why & How]
    The codes to blank all dp display have been called many times,
    so add a helper in dc_link to make it more concise.
    
    Reviewed-by: Aric Cyr <Aric.Cyr@amd.com>
    Acked-by: Solomon Chiu <solomon.chiu@amd.com>
    Signed-off-by: Leo (Hanghong) Ma <hanghong.ma@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (backported from commit 1445d967fb915156aed0e79ca80c239cb2d414ce - fix conflicts in dch110_hw_sequencer.c, dcn31_hwseq.c)
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- drivers/gpu/drm/amd/display/dc/dce110/dce110_hw_sequencer.c
+++ drivers/gpu/drm/amd/display/dc/dce110/dce110_hw_sequencer.c
@@ -1552,21 +1552,10 @@ static void power_down_encoders(struct dc *dc)
 {
 	int i;
 
-	/* do not know BIOS back-front mapping, simply blank all. It will not
-	 * hurt for non-DP
-	 */
-	for (i = 0; i < dc->res_pool->stream_enc_count; i++) {
-		dc->res_pool->stream_enc[i]->funcs->dp_blank(dc->links[i],
-					dc->res_pool->stream_enc[i]);
-	}
+	blank_all_dp_displays(dc, false);
 
 	for (i = 0; i < dc->link_count; i++) {
 		enum signal_type signal = dc->links[i]->connector_signal;
-
-		if ((signal == SIGNAL_TYPE_EDP) ||
-			(signal == SIGNAL_TYPE_DISPLAY_PORT))
-			if (!dc->links[i]->wa_flags.dp_keep_receiver_powered)
-				dp_receiver_power_ctrl(dc->links[i], false);
 
 		if (signal != SIGNAL_TYPE_EDP)
 			signal = SIGNAL_TYPE_NONE;
