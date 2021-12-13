44f28e2ccee2000c7da971876dd003d38a8232d8
John Johansen <john.johansen@canonical.com>
2021-12-13 15:46:09 -0800

    UBUNTU: SAUCE: apparmor: Add fine grained mediation of posix mqueues
    BugLink: https://bugs.launchpad.net/bugs/1989983
    
    Add fine grained mediation of posix mqueues. Specifically this patch
    adds support for differentiating mqueues based on the name in the ipc
    namespace. A follow on patch will add support for implied labels, and
    a third patch explicit labels. This is done in part because of
    dependencies on other patches to apparmor core.
    
    BugLink: https://bugs.launchpad.net/bugs/1989983
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- security/apparmor/include/ipc.h
+++ security/apparmor/include/ipc.h
@@ -11,8 +11,65 @@
 #ifndef __AA_IPC_H
 #define __AA_IPC_H
 
+#include <linux/msg.h>
 #include <linux/sched.h>
 
+#include "inode.h"
+#include "perms.h"
+
+struct aa_msg_sec {
+	struct aa_label *label;
+};
+
+struct aa_ipc_sec {
+	struct aa_label *label;
+};
+
+static inline struct aa_ipc_sec *apparmor_ipc(const struct kern_ipc_perm *ipc)
+{
+	return ipc->security + apparmor_blob_sizes.lbs_ipc;
+}
+
+static inline struct aa_msg_sec *apparmor_msg_msg(const struct msg_msg *msg_msg)
+{
+	return msg_msg->security + apparmor_blob_sizes.lbs_msg_msg;
+}
+
+
+static inline bool is_mqueue_sb(struct super_block *sb)
+{
+	if (!sb)
+		pr_warn("mqueue sb == NULL\n");
+	if (!sb && !sb->s_type->name)
+		pr_warn("mqueue sb name == NULL\n");
+	return sb && sb->s_type->name && strcmp(sb->s_type->name, "mqueue") == 0;
+}
+
+static inline bool is_mqueue_inode(struct inode *i)
+{
+	struct aa_inode_sec *isec;
+
+	if (!i)
+		return false;
+
+	isec = apparmor_inode(i);
+	return isec && isec->sclass == AA_CLASS_POSIX_MQUEUE;
+}
+
 int aa_may_signal(struct aa_label *sender, struct aa_label *target, int sig);
+
+#define AA_AUDIT_POSIX_MQUEUE_MASK (AA_MAY_WRITE | AA_MAY_READ |    \
+				    AA_MAY_CREATE | AA_MAY_DELETE | \
+				    AA_MAY_OPEN | AA_MAY_SETATTR |  \
+				    AA_MAY_GETATTR)
+
+
+int aa_profile_mqueue_perm(struct aa_profile *profile,
+			   const struct path *path,
+			   u32 request, char *buffer,
+			   struct common_audit_data *sa);
+
+int aa_mqueue_perm(const char *op, struct aa_label *label,
+		   const struct path *path, u32 request);
 
 #endif /* __AA_IPC_H */
