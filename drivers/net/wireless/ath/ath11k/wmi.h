dd8cf11506ef9d5b54dcac10ae9fea891566561b
Wen Gong <quic_wgong@quicinc.com>
2021-12-23 14:25:09 +0800

    ath11k: change to use dynamic memory for channel list of scan
    BugLink: https://bugs.launchpad.net/bugs/1955613
    
    Currently there are about 60 channels for 6 GHz, then the size of
    chan_list in struct scan_req_params which is 40 is not enough to
    fill all the channel list of 6 GHz.
    
    Use dynamic memory to save the channel list of scan.
    
    Tested-on: WCN6855 hw2.0 PCI WLAN.HSP.1.1-01720.1-QCAHSPSWPL_V1_V2_SILICONZ_LITE-1
    
    Signed-off-by: Wen Gong <quic_wgong@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20211129110939.15711-1-quic_wgong@quicinc.com
    (cherry picked from commit cea7f78d85f3f6ba05f43754600426b0e84abbbd linux)
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- drivers/net/wireless/ath/ath11k/wmi.h
+++ drivers/net/wireless/ath/ath11k/wmi.h
@@ -3025,7 +3025,6 @@ enum scan_dwelltime_adaptive_mode {
 
 #define WLAN_SCAN_MAX_NUM_SSID          10
 #define WLAN_SCAN_MAX_NUM_BSSID         10
-#define WLAN_SCAN_MAX_NUM_CHANNELS      40
 
 #define WLAN_SSID_MAX_LEN 32
 
@@ -3246,7 +3245,7 @@ struct scan_req_params {
 	u32 num_bssid;
 	u32 num_ssids;
 	u32 n_probes;
-	u32 chan_list[WLAN_SCAN_MAX_NUM_CHANNELS];
+	u32 *chan_list;
 	u32 notify_scan_events;
 	struct wlan_ssid ssid[WLAN_SCAN_MAX_NUM_SSID];
 	struct wmi_mac_addr bssid_list[WLAN_SCAN_MAX_NUM_BSSID];
