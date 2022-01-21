e6e21f7aae032d8d9c9ac4a6903f40403c17d213
Stefan Binding <sbinding@opensource.cirrus.com>
2022-01-21 17:24:24 +0000

    spi: Create helper API to lookup ACPI info for spi device
    BugLink: https://bugs.launchpad.net/bugs/1965496
    
    This can then be used to find a spi resource inside an
    ACPI node, and allocate a spi device.
    
    Signed-off-by: Stefan Binding <sbinding@opensource.cirrus.com>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://lore.kernel.org/r/20220121172431.6876-3-sbinding@opensource.cirrus.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (backported from commit 000bee0ed70af79e610444096fb453430220960f)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    Acked-by: Paolo Pisati <paolo.pisati@canonical.com>

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -17,6 +17,7 @@
 #include <linux/ptp_clock_kernel.h>
 
 #include <uapi/linux/spi/spi.h>
+#include <linux/acpi.h>
 
 struct dma_chan;
 struct software_node;
@@ -764,6 +765,10 @@ extern int devm_spi_register_controller(struct device *dev,
 extern void spi_unregister_controller(struct spi_controller *ctlr);
 
 extern struct spi_controller *spi_busnum_to_master(u16 busnum);
+#if IS_ENABLED(CONFIG_ACPI)
+extern struct spi_device *acpi_spi_device_alloc(struct spi_controller *ctlr,
+						struct acpi_device *adev);
+#endif
 
 /*
  * SPI resource management while processing a SPI message
