11a6af597f8d498dda371fa6dafb478f946113fd
Stephen Brennan <stephen.s.brennan@oracle.com>
2022-04-14 13:27:45 -0700

    UBUNTU: SAUCE: debug: Lock down kgdb
    KGDB and KDB allow read and write access to kernel memory, and thus
    should not be allowed during lockdown. An attacker with access to a
    serial port (for example, via a hypervisor console, which some cloud
    vendors provide over the network) could trigger the debugger and use it
    to bypass lockdown. Ensure KDB and KGDB cannot be used during lockdown.
    
    This fixes CVE-2022-21499.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>
    CVE-2022-21499
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Acked-by: Kamal Mostafa <kamal@canonical.com>
    Acked-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>

--- security/security.c
+++ security/security.c
@@ -60,6 +60,7 @@ const char *const lockdown_reasons[LOCKDOWN_CONFIDENTIALITY_MAX+1] = {
 	[LOCKDOWN_DEBUGFS] = "debugfs access",
 	[LOCKDOWN_XMON_WR] = "xmon write access",
 	[LOCKDOWN_BPF_WRITE_USER] = "use of bpf to write user RAM",
+	[LOCKDOWN_KGDB] = "KDB or KGDB access",
 	[LOCKDOWN_INTEGRITY_MAX] = "integrity",
 	[LOCKDOWN_KCORE] = "/proc/kcore access",
 	[LOCKDOWN_KPROBES] = "use of kprobes",
