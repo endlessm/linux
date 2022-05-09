43379f56a83fbd2995160add4ef2447f0f6b2eed
Juerg Haefliger <juerg.haefliger@canonical.com>
2022-05-09 16:25:04 +0200

    UBUNTU: [Packaging] Add module-signature-check
    BugLink: https://bugs.launchpad.net/bugs/1642368
    
    Check the signature of staging modules during package build.
    
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- debian/rules.d/4-checks.mk
+++ debian/rules.d/4-checks.mk
@@ -10,13 +10,21 @@ module-check-%: $(stampdir)/stamp-install-%
 	$(DROOT)/scripts/module-check "$*" \
 		"$(prev_abidir)" "$(abidir)" $(skipmodule)
 
+# Check the signature of staging modules
+module-signature-check-%: $(stampdir)/stamp-install-%
+	@echo Debug: $@
+	$(DROOT)/scripts/module-signature-check "$*" \
+		"$(DROOT)/$(mods_pkg_name)-$*" \
+		"$(DROOT)/$(mods_extra_pkg_name)-$*" \
+		"$(DROOT)/signature-inclusion"
+
 # Check the reptoline jmp/call functions against the last release.
 retpoline-check-%: $(stampdir)/stamp-install-%
 	@echo Debug: $@
 	$(SHELL) $(DROOT)/scripts/retpoline-check "$*" \
 		"$(prev_abidir)" "$(abidir)" "$(skipretpoline)" "$(builddir)/build-$*"
 
-checks-%: module-check-% abi-check-% retpoline-check-%
+checks-%: module-check-% module-signature-check-% abi-check-% retpoline-check-%
 	@echo Debug: $@
 
 # Check the config against the known options list.
