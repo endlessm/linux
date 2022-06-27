b6a5e89bdc12ac84be6621e27735264c21e7ca2f
Casey Schaufler <casey@schaufler-ca.com>
2022-06-27 13:54:27 -0700

    UBUNTU: SAUCE: lsm stacking v37: NET: Store LSM netlabel data in a lsmblob
    BugLink: https://bugs.launchpad.net/bugs/1989983
    
    Netlabel uses LSM interfaces requiring an lsmblob and
    the internal storage is used to pass information between
    these interfaces, so change the internal data from a secid
    to a lsmblob. Update the netlabel interfaces and their
    callers to accommodate the change. This requires that the
    modules using netlabel use the lsm_id.slot to access the
    correct secid when using netlabel.
    
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Stephen Smalley <stephen.smalley.work@gmail.com>
    Acked-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Cc: netdev@vger.kernel.org
    BugLink: https://bugs.launchpad.net/bugs/1989983
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -3729,11 +3729,12 @@ static struct smack_known *smack_from_secattr(struct netlbl_lsm_secattr *sap,
 	if ((sap->flags & NETLBL_SECATTR_CACHE) != 0)
 		return (struct smack_known *)sap->cache->data;
 
+	/*
+	 * Looks like a fallback, which gives us a secid.
+	 */
 	if ((sap->flags & NETLBL_SECATTR_SECID) != 0)
-		/*
-		 * Looks like a fallback, which gives us a secid.
-		 */
-		return smack_from_secid(sap->attr.secid);
+		return smack_from_secid(
+				sap->attr.lsmblob.secid[smack_lsmid.slot]);
 
 	if ((sap->flags & NETLBL_SECATTR_MLS_LVL) != 0) {
 		/*
@@ -4782,7 +4783,7 @@ struct lsm_blob_sizes smack_blob_sizes __lsm_ro_after_init = {
 	.lbs_superblock = sizeof(struct superblock_smack),
 };
 
-static struct lsm_id smack_lsmid __lsm_ro_after_init = {
+struct lsm_id smack_lsmid __lsm_ro_after_init = {
 	.lsm  = "smack",
 	.slot = LSMBLOB_NEEDED
 };
