e6896b9845fba7a727cb547774175a7708d8c95b
Casey Schaufler <casey@schaufler-ca.com>
2022-06-27 13:55:28 -0700

    UBUNTU: SAUCE: lsm stacking v37: LSM: security_secid_to_secctx module selection
    BugLink: https://bugs.launchpad.net/bugs/1989983
    
    Add a parameter to security_secid_to_secctx() to identify
    which of the security modules that may be active should
    provide the security context. If the parameter is greater
    than or equal to zero, the security module associated with
    that LSM "slot" is used. If the value is LSMBLOB_DISPLAY
    the "interface lsm" is used. If the value is LSMBLOB_FIRST
    the first security module providing a hook is used.
    
    The integrity IMA subsystem has chosen to always use the
    LSMBLOB_FIRST behavior, regardless of the lsm_display values.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    BugLink: https://bugs.launchpad.net/bugs/1989983
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- net/ipv4/ip_sockglue.c
+++ net/ipv4/ip_sockglue.c
@@ -140,7 +140,7 @@ static void ip_cmsg_recv_security(struct msghdr *msg, struct sk_buff *skb)
 		return;
 
 	lsmblob_init(&lb, secid);
-	err = security_secid_to_secctx(&lb, &context);
+	err = security_secid_to_secctx(&lb, &context, LSMBLOB_DISPLAY);
 	if (err)
 		return;
 
