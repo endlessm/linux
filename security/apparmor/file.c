06902838c949f982f3377864563ac7e0441fabe0
John Johansen <john.johansen@canonical.com>
2022-09-07 12:46:30 -0700

    UBUNTU: SAUCE: apparmor: af_unix mediation
    BugLink: https://bugs.launchpad.net/bugs/1989983
    
    af_socket mediation did not make it into 4.17 so add remaining out
    of tree patch
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- security/apparmor/file.c
+++ security/apparmor/file.c
@@ -14,6 +14,7 @@
 #include <linux/fs.h>
 #include <linux/mount.h>
 
+#include "include/af_unix.h"
 #include "include/apparmor.h"
 #include "include/audit.h"
 #include "include/cred.h"
@@ -228,7 +229,8 @@ int __aa_path_perm(const char *op, struct aa_profile *profile, const char *name,
 						    typeof(*rules), list);
 	int e = 0;
 
-	if (profile_unconfined(profile))
+	if (profile_unconfined(profile) ||
+	    ((flags & PATH_SOCK_COND) && !RULE_MEDIATES_AF(rules, AF_UNIX)))
 		return 0;
 	aa_str_perms(&(rules->file), rules->file.start[AA_CLASS_FILE],
 		     name, cond, perms);
