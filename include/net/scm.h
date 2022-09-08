6fbfddedc84c7dcdee228beb1235642777dcf4f7
John Johansen <john.johansen@canonical.com>
2022-09-08 09:30:09 -0700

    Revert "UBUNTU: SAUCE: LSM: Use lsmblob in security_secctx_to_secid"
    BugLink: https://bugs.launchpad.net/bugs/1989983
    
    This reverts commit 5269884b1641a326fc80cba515934ad4ab020c12.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- include/net/scm.h
+++ include/net/scm.h
@@ -97,11 +97,8 @@ static inline void scm_passec(struct socket *sock, struct msghdr *msg, struct sc
 	int err;
 
 	if (test_bit(SOCK_PASSSEC, &sock->flags)) {
-		/* There can currently be only one value in the lsmblob,
-		 * so getting it from lsmblob_value is appropriate until
-		 * security_secid_to_secctx() is converted to taking a
-		 * lsmblob directly. */
-		err = security_secid_to_secctx(lsmblob_value(&scm->lsmblob),
+		/* Scaffolding - it has to be element 0 for now */
+		err = security_secid_to_secctx(scm->lsmblob.secid[0],
 					       &secdata, &seclen);
 
 		if (!err) {
