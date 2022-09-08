e332fd8a4a7e2a45733c1e92ea9df2cc2f3a95dc
John Johansen <john.johansen@canonical.com>
2022-09-08 09:28:22 -0700

    Revert "UBUNTU: SAUCE: LSM: Use lsmcontext in security_inode_getsecctx"
    BugLink: https://bugs.launchpad.net/bugs/1989983
    
    This reverts commit bba37ef298d15c6d8982404a79c9226b9d9ed8fa.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- include/linux/security.h
+++ include/linux/security.h
@@ -583,7 +583,7 @@ void security_release_secctx(struct lsmcontext *cp);
 void security_inode_invalidate_secctx(struct inode *inode);
 int security_inode_notifysecctx(struct inode *inode, void *ctx, u32 ctxlen);
 int security_inode_setsecctx(struct dentry *dentry, void *ctx, u32 ctxlen);
-int security_inode_getsecctx(struct inode *inode, struct lsmcontext *cp);
+int security_inode_getsecctx(struct inode *inode, void **ctx, u32 *ctxlen);
 int security_locked_down(enum lockdown_reason what);
 int security_lock_kernel_down(const char *where, enum lockdown_reason level);
 #else /* CONFIG_SECURITY */
@@ -1459,8 +1459,7 @@ static inline int security_inode_setsecctx(struct dentry *dentry, void *ctx, u32
 {
 	return -EOPNOTSUPP;
 }
-static inline int security_inode_getsecctx(struct inode *inode,
-					   struct lsmcontext *cp)
+static inline int security_inode_getsecctx(struct inode *inode, void **ctx, u32 *ctxlen)
 {
 	return -EOPNOTSUPP;
 }
