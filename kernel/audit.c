b3841934181a0d0cdf23326bf130ddcfaf35bf07
Andrea Righi <andrea.righi@canonical.com>
2022-09-21 11:49:10 +0200

    Revert "UBUNTU: SAUCE: lsm stacking v37: Audit: Create audit_stamp structure"
    BugLink: https://bugs.launchpad.net/bugs/1990236
    
    This reverts commit c8abc5e842c723d0454c9e4a9e1899b5928e00af.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- kernel/audit.c
+++ kernel/audit.c
@@ -1822,11 +1822,11 @@ unsigned int audit_serial(void)
 }
 
 static inline void audit_get_stamp(struct audit_context *ctx,
-				   struct audit_stamp *stamp)
+				   struct timespec64 *t, unsigned int *serial)
 {
-	if (!ctx || !auditsc_get_stamp(ctx, stamp)) {
-		ktime_get_coarse_real_ts64(&stamp->ctime);
-		stamp->serial = audit_serial();
+	if (!ctx || !auditsc_get_stamp(ctx, t, serial)) {
+		ktime_get_coarse_real_ts64(t);
+		*serial = audit_serial();
 	}
 }
 
@@ -1849,7 +1849,8 @@ struct audit_buffer *audit_log_start(struct audit_context *ctx, gfp_t gfp_mask,
 				     int type)
 {
 	struct audit_buffer *ab;
-	struct audit_stamp stamp;
+	struct timespec64 t;
+	unsigned int serial;
 
 	if (audit_initialized != AUDIT_INITIALIZED)
 		return NULL;
@@ -1904,14 +1905,12 @@ struct audit_buffer *audit_log_start(struct audit_context *ctx, gfp_t gfp_mask,
 		return NULL;
 	}
 
-	audit_get_stamp(ab->ctx, &stamp);
+	audit_get_stamp(ab->ctx, &t, &serial);
 	/* cancel dummy context to enable supporting records */
 	if (ctx)
 		ctx->dummy = 0;
 	audit_log_format(ab, "audit(%llu.%03lu:%u): ",
-			 (unsigned long long)stamp.ctime.tv_sec,
-			 stamp.ctime.tv_nsec/1000000,
-			 stamp.serial);
+			 (unsigned long long)t.tv_sec, t.tv_nsec/1000000, serial);
 
 	return ab;
 }
