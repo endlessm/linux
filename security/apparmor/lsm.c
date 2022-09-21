99ad5a97ec77edbdf9294ae1e516875412da28d8
Andrea Righi <andrea.righi@canonical.com>
2022-09-21 11:49:31 +0200

    Revert "UBUNTU: SAUCE: upstream v6.0: apparmor: add a kernel label to use on kernel objects"
    BugLink: https://bugs.launchpad.net/bugs/1990236
    
    This reverts commit 4d0c5977ccbd3742c604e1eb5b00dde28e6b509b.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -886,7 +886,10 @@ static int apparmor_socket_post_create(struct socket *sock, int family,
 	struct aa_label *label;
 
 	if (kern) {
-		label = aa_get_label(kernel_t);
+		struct aa_ns *ns = aa_get_current_ns();
+
+		label = aa_get_label(ns_unconfined(ns));
+		aa_put_ns(ns);
 	} else
 		label = aa_get_current_label();
 
--- security/apparmor/policy_ns.c
+++ security/apparmor/policy_ns.c
@@ -22,9 +22,6 @@
 #include "include/label.h"
 #include "include/policy.h"
 
-/* kernel label */
-struct aa_label *kernel_t;
-
 /* root profile namespace */
 struct aa_ns *root_ns;
 const char *aa_hidden_ns_name = "---";
@@ -80,23 +77,6 @@ const char *aa_ns_name(struct aa_ns *curr, struct aa_ns *view, bool subns)
 	return aa_hidden_ns_name;
 }
 
-struct aa_profile *alloc_unconfined(const char *name)
-{
-	struct aa_profile *profile;
-
-	profile = aa_alloc_profile(name, NULL, GFP_KERNEL);
-	if (!profile)
-		return NULL;
-
-	profile->label.flags |= FLAG_IX_ON_NAME_ERROR |
-		FLAG_IMMUTIBLE | FLAG_NS_COUNT | FLAG_UNCONFINED;
-	profile->mode = APPARMOR_UNCONFINED;
-	profile->file.dfa = aa_get_dfa(nulldfa);
-	profile->policy.dfa = aa_get_dfa(nulldfa);
-
-	return profile;
-}
-
 /**
  * alloc_ns - allocate, initialize and return a new namespace
  * @prefix: parent namespace name (MAYBE NULL)
@@ -121,9 +101,16 @@ static struct aa_ns *alloc_ns(const char *prefix, const char *name)
 	init_waitqueue_head(&ns->wait);
 
 	/* released by aa_free_ns() */
-	ns->unconfined = alloc_unconfined("unconfined");
+	ns->unconfined = aa_alloc_profile("unconfined", NULL, GFP_KERNEL);
 	if (!ns->unconfined)
 		goto fail_unconfined;
+
+	ns->unconfined->label.flags |= FLAG_IX_ON_NAME_ERROR |
+		FLAG_IMMUTIBLE | FLAG_NS_COUNT | FLAG_UNCONFINED;
+	ns->unconfined->mode = APPARMOR_UNCONFINED;
+	ns->unconfined->file.dfa = aa_get_dfa(nulldfa);
+	ns->unconfined->policy.dfa = aa_get_dfa(nulldfa);
+
 	/* ns and ns->unconfined share ns->unconfined refcount */
 	ns->unconfined->ns = ns;
 
@@ -401,21 +388,10 @@ static void __ns_list_release(struct list_head *head)
  */
 int __init aa_alloc_root_ns(void)
 {
-	struct aa_profile *kernel_p;
-
 	/* released by aa_free_root_ns - used as list ref*/
 	root_ns = alloc_ns(NULL, "root");
 	if (!root_ns)
 		return -ENOMEM;
-
-	kernel_p = alloc_unconfined("kernel_t");
-	if (!kernel_p) {
-		destroy_ns(root_ns);
-		aa_free_ns(root_ns);
-		return -ENOMEM;
-	}
-	kernel_t = &kernel_p->label;
-	root_ns->unconfined->ns = aa_get_ns(root_ns);
 
 	return 0;
 }
@@ -429,7 +405,6 @@ void __init aa_free_root_ns(void)
 
 	 root_ns = NULL;
 
-	 aa_label_free(kernel_t);
 	 destroy_ns(ns);
 	 aa_put_ns(ns);
 }
