a4778f291257ae38c7da03fb9786e77113a86ce1
John Johansen <john.johansen@canonical.com>
2022-09-26 10:21:42 -0700

    Revert "Revert "Revert "UBUNTU: SAUCE: apparmor: af_unix mediation"""
    BugLink: https://bugs.launchpad.net/bugs/1989983
    
    This reverts commit fb18a7de0eda532bf797981d996802c46c0a53e0.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- security/apparmor/file.c
+++ security/apparmor/file.c
@@ -14,7 +14,6 @@
 #include <linux/fs.h>
 #include <linux/mount.h>
 
-#include "include/af_unix.h"
 #include "include/apparmor.h"
 #include "include/audit.h"
 #include "include/cred.h"
@@ -272,8 +271,7 @@ int __aa_path_perm(const char *op, struct aa_profile *profile, const char *name,
 {
 	int e = 0;
 
-	if (profile_unconfined(profile) ||
-	    ((flags & PATH_SOCK_COND) && !PROFILE_MEDIATES_AF(profile, AF_UNIX)))
+	if (profile_unconfined(profile))
 		return 0;
 	aa_str_perms(profile->file.dfa, profile->file.start, name, cond, perms);
 	if (request & ~perms->allow)
