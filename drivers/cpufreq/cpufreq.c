d3cc1b9084589edf17f40a8bd0476f3f11e6b201
Paolo Pisati <paolo.pisati@canonical.com>
2024-01-23 16:28:52 +0100

    UBUNTU: SAUCE: cpufreq: s/strlcpy/strscpy/
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2889,7 +2889,7 @@ static char cpufreq_driver_name[CPUFREQ_NAME_LEN];
 
 static int __init cpufreq_driver_setup(char *str)
 {
-	strlcpy(cpufreq_driver_name, str, CPUFREQ_NAME_LEN);
+	strscpy(cpufreq_driver_name, str, CPUFREQ_NAME_LEN);
 	return 1;
 }
 
