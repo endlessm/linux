e243596e89613940c0f7c55f4e1f1c634558f87b
Timo Aaltonen <timo.aaltonen@canonical.com>
2024-08-08 16:13:24 +0300

    UBUNTU: [Packaging] Disable tools/perf on armhf (FTBFS)
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- debian.master/rules.d/armhf.mk
+++ debian.master/rules.d/armhf.mk
@@ -9,9 +9,9 @@ no_dumpfile	= true
 
 do_tools_usbip  = true
 do_tools_cpupower = true
-do_tools_perf	= true
-do_tools_perf_jvmti = true
-do_tools_perf_python = true
+do_tools_perf	= false
+do_tools_perf_jvmti = false
+do_tools_perf_python = false
 do_tools_bpftool = true
 do_tools_rtla = true
 
