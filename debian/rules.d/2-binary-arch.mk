1855cd2af4aeb9733edafe278c94c46c37986536
Luca Boccassi <bluca@debian.org>
2024-08-22 18:10:56 +0100

    UBUNTU: [Packaging] add linux-bpf-dev package
    BugLink: https://bugs.launchpad.net/bugs/2050083
    
    Add a new binary linux-bpf-dev package that ships a generated vmlinux.h
    header. This can be used by packages building CO-RE BPF programs instead
    of generating it at build time based on the kernel running on the package
    build system, which might or might not match the kernel that is actually
    shipped. It ensures that a matching header can always be used.
    
    Matches changes in Debian, including the package name and header installation
    location, so that we do not have to ifdef:
    
    https://salsa.debian.org/kernel-team/linux/-/commit/f52d006f3915ac4358dc8c98aa417477ebee026e
    
    This is already used by systemd to build various CO-RE programs, and
    right now on Ubuntu due to the lack of this package the header is
    generated from sysfs in the build system where the systemd packages are
    built.
    
    Signed-off-by: Luca Boccassi <bluca@debian.org>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -616,6 +616,9 @@ endif
 ifeq ($(do_cloud_tools),true)
 	$(call dh_all,$(pkgcloud))
 endif
+ifeq ($(do_tools_bpftool),true)
+	$(call dh_all,linux-bpf-dev)
+endif
 
 #
 # per-architecture packages
@@ -661,6 +664,7 @@ endif
 ifeq ($(do_tools_bpftool),true)
 	mv $(builddirpa)/tools/bpf/bpftool/vmlinux $(builddirpa)/vmlinux
 	$(kmake) NO_LIBTRACEEVENT=1 CROSS_COMPILE=$(CROSS_COMPILE) -C $(builddirpa)/tools/bpf/bpftool
+	$(builddirpa)/tools/bpf/bpftool/bpftool btf dump file $(builddirpa)/vmlinux format c > $(builddirpa)/vmlinux.h
 	rm -f $(builddirpa)/vmlinux
 endif
 ifeq ($(do_tools_x86),true)
@@ -712,6 +716,10 @@ endif
 endif
 ifeq ($(do_tools_bpftool),true)
 	install -m755 $(builddirpa)/tools/bpf/bpftool/bpftool $(toolspkgdir)/usr/lib/$(src_pkg_name)-tools-$(abi_release)
+endif
+ifeq ($(do_tools_bpftool),true)
+	install -d -m755 $(CURDIR)/debian/linux-bpf-dev/usr/include/$(DEB_HOST_MULTIARCH)/linux/
+	install -m644 $(builddirpa)/vmlinux.h $(CURDIR)/debian/linux-bpf-dev/usr/include/$(DEB_HOST_MULTIARCH)/linux/vmlinux.h
 endif
 ifeq ($(do_tools_x86),true)
 	install -m755 \
