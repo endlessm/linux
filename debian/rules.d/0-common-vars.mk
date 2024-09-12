b95d8044ae921f130921e708ebec5b7d0d93c5be
Timo Aaltonen <timo.aaltonen@canonical.com>
2024-09-12 11:43:58 +0300

    UBUNTU: [Packaging] Don't force bindgen version
    The kernel supports multiple versions now.
    
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- debian/rules.d/0-common-vars.mk
+++ debian/rules.d/0-common-vars.mk
@@ -197,7 +197,6 @@ kmake = make ARCH=$(build_arch) \
 	CROSS_COMPILE=$(CROSS_COMPILE) \
 	HOSTCC=$(HOSTCC) \
 	CC=$(CROSS_COMPILE)$(gcc) \
-	BINDGEN=bindgen-0.65 \
 	KERNELRELEASE=$(abi_release)-$(target_flavour) \
 	CONFIG_DEBUG_SECTION_MISMATCH=y \
 	KBUILD_BUILD_VERSION="$(uploadnum)" \
