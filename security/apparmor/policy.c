74c6ea6afed01d651f50afe35525aeb82e3dff64
John Johansen <john.johansen@canonical.com>
2025-08-03 22:07:52 -0700

    UBUNTU: SAUCE: apparmor5.0.0 [35/51]: apparmor: guard against free routines being called with a NULL
    BugLink: http://bugs.launchpad.net/bugs/2028253
    
    aa_free_data() and free_attachment() don't guard against having
    a NULL parameter passed to them. Fix this.
    
    Reviewed-by: Ryan Lee <ryan.lee@canonical.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- security/apparmor/policy.c
+++ security/apparmor/policy.c
@@ -236,6 +236,9 @@ static void aa_free_data(void *ptr, void *arg)
 {
 	struct aa_data *data = ptr;
 
+	if (!ptr)
+		return;
+
 	kvfree_sensitive(data->data, data->size);
 	kfree_sensitive(data->key);
 	kfree_sensitive(data);
@@ -244,6 +247,9 @@ static void aa_free_data(void *ptr, void *arg)
 static void free_attachment(struct aa_attachment *attach)
 {
 	int i;
+
+	if (!attach)
+		return;
 
 	for (i = 0; i < attach->xattr_count; i++)
 		kfree_sensitive(attach->xattrs[i]);
